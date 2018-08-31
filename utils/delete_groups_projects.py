c7eb55a16a4378831e0d306ce86f621e528e112e
fvivian <f.vivian@hotmail.com>
2018-08-31 14:35:30 +0000

    include deleting mysql projects

--- utils/delete_groups_projects.py
+++ utils/delete_groups_projects.py
@@ -1,6 +1,5 @@
 #!/bin/python3
 # -*- coding: UTF-8 -*-
-# Author: M. Reinmuth, B. Herfort
 ########################################################################################################################
 
 import sys
@@ -16,20 +15,36 @@ parser.add_argument('-mo', '--modus', nargs='?', default='development', choices=
 
 ########################################################################################################################
 
-def delete_groups_projects():
+def delete_groups_projects_firebase():
 
     firebase = firebase_admin_auth()
     fb_db = firebase.database()
     
     
-    print('Deleting all groups...')
+    print('Deleting all groups from Firebase...')
     fb_db.child("groups").remove()
     print('Done.')
 
-    print('Deleting all projects...')
+    print('Deleting all projects from Firebase...')
     fb_db.child("projects").remove()
     print('Done.')
 
+
+
+def delete_projects_mysql():
+    print('Delete all projects from MySQL...')
+    try:
+        m_con = mysqlDB()
+        sql_insert = "DELETE FROM projects"
+        m_con.query(sql_insert, None)
+        del(m_con)
+
+        print('Done')
+        return True
+
+    except Exception as e:
+        print(e)
+        return False
 
 if __name__ == '__main__':
 
@@ -41,10 +56,13 @@ if __name__ == '__main__':
     if args.modus == 'development':
         # we use the dev instance for testing
         from cfg.auth import dev_firebase_admin_auth as firebase_admin_auth
+        from cfg.auth import dev_mysqlDB as mysqlDB
         print('We are using the development instance')
     elif args.modus == 'production':
         # we use the dev instance for testing
         from cfg.auth import firebase_admin_auth as firebase_admin_auth
+        from cfg.auth import mysqlDB as mysqlDB
         print('We are using the production instance')
 
-delete_groups_projects()
+delete_groups_projects_firebase()
+delete_projects_mysql()
