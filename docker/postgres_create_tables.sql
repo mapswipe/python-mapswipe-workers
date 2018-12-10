804b4d577a48d7436875e0359af58fae92274615
marcel <marcelr93@gmail.com>
2018-12-10 20:16:55 +0100

    update on postgres table structure

--- docker/postgres_create_tables.sql
+++ docker/postgres_create_tables.sql
@@ -1,9 +1,33 @@
+-- noinspection SqlNoDataSourceInspectionForFile
+
 CREATE TABLE IF NOT EXISTS projects (
-  project_id int
-  ,project_type int
+  contributors int
+  ,groupAverage int
+  ,project_id int
+  ,image varchar
+  ,importKey varchar
+  ,isFeatured boolean
+  ,lookFor varchar
   ,name varchar
-  ,objective varchar
+  ,progress int
+  ,projectDetails varchar
+  ,state int
+  ,verificationCount int
+  ,zoom int
+  ,project_type int
+
   ,CONSTRAINT projects_pkey PRIMARY KEY(project_id)
+);
+
+CREATE TABLE IF NOT EXISTS imports (
+  import_id varchar
+  ,complete boolean
+  ,key varchar
+  ,kml varchar
+  ,project_id int
+  ,tileServer varchar
+
+  ,CONSTRAINT imports_pkey PRIMARY KEY(import_id)
 );
 
 CREATE TABLE IF NOT EXISTS tasks (
@@ -11,7 +35,26 @@ CREATE TABLE IF NOT EXISTS tasks (
   ,group_id int
   ,project_id int
   ,info json
+
   ,CONSTRAINT tasks_pkey PRIMARY KEY(task_id, group_id, project_id)
+);
+
+CREATE TABLE IF NOT EXISTS groups (
+  project_id int
+  ,group_id int
+  ,completedCount int
+  ,distributedCount int
+
+  ,CONSTRAINT groups_pkeay PRIMARY KEY(project_id, group_id)
+);
+
+CREATE TABLE IF NOT EXISTS users (
+  user_id varchar
+  ,contributions int
+  ,distance int
+  ,username varchar
+
+  ,CONSTRAINT users_pkeay PRIMARY KEY(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS results (
@@ -21,6 +64,16 @@ CREATE TABLE IF NOT EXISTS results (
   ,"timestamp" bigint
   ,info json
   ,duplicates integer
+
   ,CONSTRAINT results_pkey PRIMARY KEY (task_id, user_id, project_id)
 );
+
+CREATE TABLE IF NOT EXISTS progress(
+  project_id int
+  ,contributions int
+  ,progress int
+  ,timestamp bigint
+
+);
+
 
