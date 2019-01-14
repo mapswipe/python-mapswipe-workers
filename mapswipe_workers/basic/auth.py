f668e285de975ed19302f4b8664c735c4e985952
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-01-14 15:38:15 +0100

    changed config path

--- mapswipe_workers/basic/auth.py
+++ mapswipe_workers/basic/auth.py
@@ -11,12 +11,12 @@ import sys
 import os.path
 import errno
 
-if os.path.isfile('./cfg/config.cfg'):
-    CONFIG_PATH = './cfg/config.cfg'
-elif os.path.isfile('./cfg/your_config_file.cfg'):
-    CONFIG_PATH = './cfg/your_config_file.cfg'
+if os.path.isfile('../../cfg/config.cfg'):
+    CONFIG_PATH = '../../cfg/config.cfg'
+elif os.path.isfile('../../cfg/your_config_file.cfg'):
+    CONFIG_PATH = '../../cfg/your_config_file.cfg'
 else:
-    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), './cfg/config.cfg')
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), '../../cfg/config.cfg')
     
 # try:
 #     CONFIG_PATH = './cfg/config.cfg'
