231a9c431ca68ce972551a2a0f42c9ced0d372d2
h9b <herfort@uni-heidelberg.de>
2019-01-22 15:00:33 +0100

    docu for Tile functions #52

--- mapswipe_workers/ProjectTypes/BuildArea/TileFunctions.py
+++ mapswipe_workers/ProjectTypes/BuildArea/TileFunctions.py
@@ -3,19 +3,57 @@ import ogr
 
 
 class Point:
+    """
+    The basic class point representing a Pixel
+
+    Attributes
+    ----------
+    x : int
+        x coordinate
+    y : int
+        y coordinate
+    """
+
     def __init__(self, x=0.0, y=0.0):
         self.x = x
         self.y = y
 
 
 class Tile:
+    """
+    The basic class tile representing a TMS tile
+
+    Attributes
+    ----------
+    x : int
+        x coordinate
+    y : int
+        y coordinate
+    """
+
     def __init__(self, x=0, y=0):
         self.x = x
         self.y = y
 
 
 def lat_long_zoom_to_pixel_coords(lat, lon, zoom):
-    """Create pixel coordinates from lat-long point at a given zoom level"""
+    """
+    The function to compute pixel coordinates from lat-long point at a given zoom level
+
+    Parameters
+    ----------
+    lat : float
+        latitude in degree
+    lon : float
+        longitude in degree
+    zoom : int
+        tile map service zoom level
+
+    Returns
+    -------
+    p : Point
+    """
+
     p = Point()
     sinLat = math.sin(lat * math.pi / 180.0)
     x = ((lon + 180) / 360) * 256 * math.pow(2, zoom)
@@ -27,6 +65,27 @@ def lat_long_zoom_to_pixel_coords(lat, lon, zoom):
 
 
 def pixel_coords_zoom_to_lat_lon(PixelX, PixelY, zoom):
+    """
+    The function to compute latitude, longituted from pixel coordinates at a given zoom level
+
+    Parameters
+    ----------
+    PixelX : int
+        x coordinate
+    PixelY : int
+        y coordinate
+    zoom : int
+        tile map service zoom level
+
+    Returns
+    -------
+    lon : float
+        the longitude in degree
+    lat : float
+        the latitude in degree
+    """
+
+
     MapSize = 256 * math.pow(2, zoom)
     x = (PixelX / MapSize) - 0.5
     y = 0.5 - (PixelY / MapSize)
@@ -36,63 +95,148 @@ def pixel_coords_zoom_to_lat_lon(PixelX, PixelY, zoom):
     return lon, lat
 
 
-def pixel_coords_to_tile_address(x, y):
-    """Create a tile address from pixel coordinates of point within tile."""
+def pixel_coords_to_tile_address(PixelX, PixelY):
+    """
+    The function to compute a tile address from pixel coordinates of point within tile.
+
+    Parameters
+    ----------
+    PixelX : int
+        x coordinate
+    PixelY : int
+        y coordinate
+
+    Returns
+    -------
+    t : Tile
+    """
+
     t = Tile()
-    t.x = int(math.floor(x / 256))
-    t.y = int(math.floor(y / 256))
+    t.x = int(math.floor(PixelX / 256))
+    t.y = int(math.floor(PixelY / 256))
     return t
 
 
-def tile_coords_zoom_and_tileserver_to_URL(TileX, TileY, zoomlevel, tileserver,
+def tile_coords_zoom_and_tileserver_to_URL(TileX, TileY, zoom, tileserver,
                                            api_key, custom_tileserver_url, wmts_layer):
-    """Create a URL for a tile based on tile coordinates and zoom"""
+    """
+    The function to Create a URL for a tile based on tile coordinates and zoom
+
+    Parameters
+    ----------
+    TileX : int
+        x coordinate of tile
+    TileY : int
+        y coordinate of tile
+    zoom :  int
+        tile map service zoom level
+    tileserver :  string
+        the name of the tile server, can bei either `digital_globe`, `google`, `sinergise` or `custom`
+    api_key : string
+        the api key to access the given tile server
+    custom_tileserver_url : string
+        the url for a custom tile server with {x}, {y}, {z} place holders
+    wmts_layer : sting
+        the name of the layer for a given wmts service url, e.g. when using sinergise
+
+    Returns
+    -------
+    URL : string
+        the url for the specific tile image
+    """
+
     URL = ''
     if tileserver == 'bing':
         quadKey = tile_coords_and_zoom_to_quadKey(
-            int(TileX), int(TileY), int(zoomlevel))
+            int(TileX), int(TileY), int(zoom))
         URL = quadKey_to_Bing_URL(quadKey, api_key)
     elif tileserver == 'digital_globe':
         URL = ("https://api.mapbox.com/v4/digitalglobe.nal0g75k/"
                "{}/{}/{}.png?access_token={}"
-               .format(zoomlevel, TileX, TileY, api_key))
+               .format(zoom, TileX, TileY, api_key))
     elif tileserver == 'google':
         URL = ("https://mt0.google.com/vt/lyrs=s&hl=en&x={}&y={}&z={}"
-               .format(TileX, TileY, zoomlevel))
+               .format(TileX, TileY, zoom))
     elif tileserver == 'sinergise':
         URL = ("https://services.sentinel-hub.com/ogc/wmts/{}?request=getTile&tilematrixset=PopularWebMercator256&tilematrix={}&tilecol={}&tilerow={}&layer={}".format(
-            api_key, zoomlevel, TileX, TileY, wmts_layer))
+            api_key, zoom, TileX, TileY, wmts_layer))
 
     elif tileserver == 'custom':
-        # don't forget the linebreak!
-        URL = custom_tileserver_url.format(z=zoomlevel, x=TileX, y=TileY)
+        URL = custom_tileserver_url.format(z=zoom, x=TileX, y=TileY)
     return URL
 
 
-def tile_coords_and_zoom_to_quadKey(x, y, zoom):
-    """Create a quadkey for use with certain tileservers that use them."""
+def tile_coords_and_zoom_to_quadKey(TileX, TileY, zoom):
+    """
+    The function to create a quadkey for use with certain tileservers that use them, e.g. Bing
+
+    Parameters
+    ----------
+    TileX : int
+         x coordinate of tile
+    TileY : int
+         y coordinate of tile
+    zoom : int
+        tile map service zoom level
+
+    Returns
+    -------
+    quadKey : str
+    """
+
     quadKey = ''
     for i in range(zoom, 0, -1):
         digit = 0
         mask = 1 << (i - 1)
-        if (x & mask) != 0:
+        if (TileX & mask) != 0:
             digit += 1
-        if (y & mask) != 0:
+        if (TileY & mask) != 0:
             digit += 2
         quadKey += str(digit)
     return quadKey
 
 
 def quadKey_to_Bing_URL(quadKey, api_key):
-    """Create a URL linking to a Bing tile server"""
+    """
+    The function to create a tile image URL linking to a Bing tile server
+
+    Parameters
+    ----------
+    quadKey :  str
+        the quad key for Bing for the given tile
+    api_key : str
+        the Bing maps api key
+
+    Returns
+    -------
+    tile_url : str
+        the url for the specific Bing tile image
+    """
+
     tile_url = ("http://t0.tiles.virtualearth.net/tiles/a{}.jpeg?"
                 "g=854&mkt=en-US&token={}".format(quadKey, api_key))
-    #print "\nThe tile URL is: {}".format(tile_url)
 
     return tile_url
 
 
 def geometry_from_tile_coords(TileX, TileY, zoom):
+    """
+    The function to compute the polygon geometry of a tile map service tile
+
+    Parameters
+    ----------
+    TileX : int
+         x coordinate of tile
+    TileY : int
+         y coordinate of tile
+    zoom : int
+        tile map service zoom level
+
+    Returns
+    -------
+    wkt_geom : str
+        the polygon geometry of the tile in WKT format
+    """
 
     # Calculate lat, lon of upper left corner of tile
     PixelX = TileX * 256
