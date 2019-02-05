51eef197508117d4994e93da9d3c7613abe70137
Matthias <matthias@dhcp561-042.laptop-wlc.uni-heidelberg.de>
2019-02-05 16:54:22 +0100

    corrected path handling

--- mapswipe_workers/definitions.py
+++ mapswipe_workers/definitions.py
@@ -1,7 +1,5 @@
 import os
 
-#ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-ROOT_DIR = '/home/matthias/Arbeit/projekte/python-mapswipe-workers/mapswipe_workers'
-#CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'cfg', 'configuration.json'))
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'cfg', 'configuration.json'))
 DATA_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'data'))
