23ba5d33d3d5ddc95b1261da1a530a73a43fc395
Matthias <matthias@dhcp561-042.laptop-wlc.uni-heidelberg.de>
2019-02-05 11:08:37 +0100

    writing tests

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,7 +1,10 @@
 import json
 from mapswipe_workers.definitions import CONFIG_PATH
 
-with open(CONFIG_PATH) as f:
-    config = f.read
+def upload_sample_data_to_firebase():
+    with open(CONFIG_PATH) as f:
+        config = json.load(f)
+    print(config)
 
-
+if __name__ == '__main__':
+    upload_sample_data_to_firebase()
