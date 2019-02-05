0df8392cb6979633ae97ff280ab79fc5346feac2
Matthias <matthias@dhcp561-042.laptop-wlc.uni-heidelberg.de>
2019-02-05 13:51:01 +0100

    writing tests

--- mapswipe_workers/definitions.py
+++ mapswipe_workers/definitions.py
@@ -1,5 +1,6 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'cfg', 'configuration.json'))
+#CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'cfg', 'configuration.json'))
+CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, 'configuration.json'))
 DATA_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'data'))
--- tests/test_init.py
+++ tests/test_init.py
@@ -1,10 +1,10 @@
 import json
-from mapswipe_workers.definitions import CONFIG_PATH
+from mapswipe_workers.basic import BaseFunctions
+
 
 def upload_sample_data_to_firebase():
-    with open(CONFIG_PATH) as f:
-        config = json.load(f)
-    print(config)
+    firebase, postgres = BaseFunctions.get_environment('development')
+
 
 if __name__ == '__main__':
     upload_sample_data_to_firebase()
