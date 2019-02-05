bd276d03c0f2384067b25db1d3ff50cf654abc4f
Matthias <matthias@dhcp561-042.laptop-wlc.uni-heidelberg.de>
2019-02-05 11:12:23 +0100

    corected path handling

--- mapswipe_workers/definitions.py
+++ mapswipe_workers/definitions.py
@@ -1,5 +1,5 @@
 import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-CONFIG_PATH = os.path.join(ROOT_DIR, '../cfg/configuration.json')
-DATA_PATH = os.path.join(ROOT_DIR, '../data/')
+CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'cfg'))
+DATA_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'data'))
