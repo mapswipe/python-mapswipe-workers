9f62bf4d266a30724ccc7c2744ffc684b2fb8f98
Matthias <schaub@stud.uni-heidelberg.de>
2019-02-06 16:35:23 +0100

    added custom error

--- mapswipe_workers/definitions.py
+++ mapswipe_workers/definitions.py
@@ -3,3 +3,6 @@ import os
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'cfg', 'configuration.json'))
 DATA_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'data'))
+
+class CustomError(Exception):
+    pass
