502f6d972a00e442dd6cdc9a912e805a9c3151f2
Herfort <h9b@geog>
2019-02-19 13:48:18 +0100

    add scripts to test for old projects in production

---
+++ tests/test_update_old_projects.py
@@ -0,0 +1,10 @@
+from mapswipe_workers.basic import BaseFunctions
+
+def test_update_old_project():
+
+    project_ids = [3, 124, 5519, 13523]
+    BaseFunctions.run_update('development', project_ids)
+
+
+if __name__ == '__main__':
+    test_update_old_project()
