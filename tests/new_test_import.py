1a0371addc7323c6d31fc1999764863219099b5e
Matthias <matthias@dhcp663-092.laptop-wlc.uni-heidelberg.de>
2019-03-21 12:43:04 +0100

    Support the new Firebase Realtime Database structure #104

--- tests/new_test_import.py
+++ tests/new_test_import.py
@@ -5,17 +5,17 @@ from mapswipe_workers.basic import BaseFunctions
 
 
 def test_import_process():
-    imported_projects = BaseFunctions.run_import()
+    imported_projects = BaseFunctions.run_import('production')
 
     # save all keys to disk
-    filename = 'firebase_imported_projects.pickle'
+    filename = 'imported_project_ids.pickle'
     if os.path.isfile(filename):
         with open(filename, 'rb') as f:
-            already_imported_projects = pickle.load(f)
-        imported_projects = imported_projects + already_imported_projects
+            already_imported_project_ids = pickle.load(f)
+        imported_project_ids = imported_project_ids + already_imported_project_ids
 
     with open(filename, 'wb') as f:
-        pickle.dump(imported_projects, f)
+        pickle.dump(imported_project_ids, f)
 
 
 if __name__ == '__main__':
