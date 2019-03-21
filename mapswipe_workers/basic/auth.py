ea163cced7d650da2ce7a9ef1dda129274ace4fb
Matthias <matthias@dhcp663-092.laptop-wlc.uni-heidelberg.de>
2019-03-21 12:44:35 +0100

    renamed psqlDB to postgresDB for consistancy (firebaseDB)

--- mapswipe_workers/basic/auth.py
+++ mapswipe_workers/basic/auth.py
@@ -37,7 +37,7 @@ def firebaseDB():
 
     # Initialize the app with a service account, granting admin privileges
     firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://test-mapswipe.firebaseio.com'
+        'databaseURL': 'https://dev-mapswipe.firebaseio.com'
     })
 
     return db
@@ -58,7 +58,6 @@ def firebase_admin_auth():
         # Default Configuration
         raise Exception('could not get firebase informtaion from config file')
 
-    service_key_path = os.path.abspath(os.path.join(ROOT_DIR, 'cfg', service_account))
     # adapt this to your firebase setting
     config = {
         "apiKey": api_key,
@@ -66,7 +65,7 @@ def firebase_admin_auth():
         "databaseURL": database_url,
         "storageBucket": storage_bucket,
         # this is important if you want to login as admin
-        "serviceAccount": service_key_path
+        "serviceAccount": SERVICE_ACCOUNT_KEY_PATH
     }
     firebase = pyrebase.initialize_app(config)
     return firebase
@@ -85,7 +84,6 @@ def dev_firebase_admin_auth():
         # Default Configuration
         raise Exception('Could not get firebase dev information from config file.')
 
-    service_key_path = os.path.abspath(os.path.join(ROOT_DIR, 'cfg', service_account))
     # adapt this to your firebase setting
     config = {
         "apiKey": api_key,
@@ -93,7 +91,7 @@ def dev_firebase_admin_auth():
         "databaseURL": database_url,
         "storageBucket": storage_bucket,
         # this is important if you want to login as admin
-        "serviceAccount": service_key_path
+        "serviceAccount": SERVICE_ACCOUNT_KEY_PATH
     }
 
     print('use dev configuration: %s' % config)
@@ -188,6 +186,52 @@ class dev_psqlDB(object):
 
 
 class psqlDB(object):
+    _db_connection = None
+    _db_cur = None
+
+    def __init__(self):
+        # try to load configuration from config file
+        CONFIG = load_config()
+        try:
+            dbname = CONFIG['psql']['database']
+            user = CONFIG['psql']['username']
+            password = CONFIG['psql']['password']
+            host = CONFIG['psql']['host']
+            port = CONFIG['psql']['port']  # print('use configuration for psql as provided by config.json')
+        except:
+            # Default configuration
+            raise Exception('Could not load psql info from the config file')
+
+        self._db_connection = psycopg2.connect(database=dbname, user=user, password=password, host=host, port=port)
+
+    def query(self, query, data):
+        self._db_cur = self._db_connection.cursor()
+        self._db_cur.execute(query, data)
+        self._db_connection.commit()
+        self._db_cur.close()
+        return
+
+    def retr_query(self, query, data):
+        self._db_cur = self._db_connection.cursor()
+        self._db_cur.execute(query, data)
+        content = self._db_cur.fetchall()
+        self._db_connection.commit()
+        self._db_cur.close()
+        return content
+
+    def copy_from(self, file, table, sep='\t', null='\\N', size=8192, columns=None):
+        self._db_cur = self._db_connection.cursor()
+        self._db_cur.copy_from(file, table, sep=sep, null='\\N', size=8192, columns=columns)
+        self._db_connection.commit()
+        self._db_cur.close()
+        return
+
+    def __del__(self):
+        # self._db_cur.close()
+        self._db_connection.close()
+
+
+class postgresDB(object):
     _db_connection = None
     _db_cur = None
 
