5e63e00fb55b7f7d4813ecde5bd18d781d535cdb
Matthias <matthias@dhcp565-198.laptop-wlc.uni-heidelberg.de>
2019-03-27 11:43:51 +0100

    Improve database schema #99

--- docker/postgres_create_tables.sql
+++ docker/postgres_create_tables.sql
@@ -1,88 +1,87 @@
 -- noinspection SqlNoDataSourceInspectionForFile
 
 CREATE TABLE IF NOT EXISTS projects (
-  contributors int
-  ,groupAverage int
-  ,project_id int
-  ,image varchar
-  ,importKey varchar
-  ,isFeatured boolean
-  ,lookFor varchar
-  ,name varchar
-  ,progress int
-  ,projectDetails varchar
-  ,state int
-  ,verificationCount int
-  ,project_type int
-  ,info json
-  ,archive boolean
+    project_id int,
+    name varchar,
+    project_type int,
+    project_details varchar,
+    group_average int,
+    contributors int,
+    image varchar,
+    is_featured boolean,
+    look_for varchar,
+    progress int,
+    state int,
+    verification_count int,
+    archive boolean,
+    project_type_specifics json,
+    PRIMARY KEY(project_id)
+    );
 
-  ,CONSTRAINT projects_pkey PRIMARY KEY(project_id)
-);
 
-CREATE TABLE IF NOT EXISTS imports (
-  import_id varchar
-  ,info json
+CREATE TABLE IF NOT EXISTS groups (
+    project_id int,
+    group_id int,
+    count int,
+    completed_count int,
+    verification_count int,
+    project_type_specifics json,
+    PRIMARY KEY(project_id, group_id)
+    FOREIGN KEY (project_id) REFERENCES projects (project_id)
+    );
 
-  ,CONSTRAINT imports_pkey PRIMARY KEY(import_id)
-);
+CREATE INDEX groups_projectid ON public.groups USING btree (group_id);
+CREATE INDEX groups_goupid ON public.groups USING btree (project_id);
+
 
 CREATE TABLE IF NOT EXISTS tasks (
-  task_id varchar
-  ,group_id int
-  ,project_id int
-  ,info json
-
-  ,CONSTRAINT tasks_pkey PRIMARY KEY(task_id, group_id, project_id)
-);
+    project_id int,
+    group_id int,
+    task_id varchar,
+    project_type_specifics json,
+    PRIMARY KEY(project_id, group_id, task_id)
+    FOREIGN KEY (project_id) REFERENCES projects (project_id)
+    FOREIGN KEY (group_id) REFERENCES groups (group_id)
+    );
 
 CREATE INDEX tasks_task_id ON public.tasks USING btree (task_id);
 CREATE INDEX tasks_groupid ON public.tasks USING btree (group_id);
 CREATE INDEX tasks_projectid ON public.tasks USING btree (project_id);
 
-CREATE TABLE IF NOT EXISTS groups (
-  project_id int
-  ,group_id int
-  ,count int
-  ,completedCount int
-  ,verificationCount int
-  ,info json
-
-  ,CONSTRAINT groups_pkey PRIMARY KEY(project_id, group_id)
-);
-
-CREATE INDEX groups_projectid ON public.groups USING btree (group_id);
-CREATE INDEX groups_goupid ON public.groups USING btree (project_id);
 
 CREATE TABLE IF NOT EXISTS users (
-  user_id varchar
-  ,contributions int
-  ,distance double precision
-  ,username varchar
+    user_id varchar,
+    contributions int,
+    distance double precision,
+    username varchar,
+    PRIMARY KEY(user_id)
+    );
 
-  ,CONSTRAINT users_pkey PRIMARY KEY(user_id)
-);
 
 CREATE TABLE IF NOT EXISTS results (
-  task_id character varying NOT NULL
-  ,project_id integer NOT NULL
-  ,user_id character varying NOT NULL
-  ,"timestamp" bigint
-  ,info json
-  ,duplicates integer
-
-  ,CONSTRAINT results_pkey PRIMARY KEY (task_id, user_id, project_id)
+    task_id character varying NOT NULL,
+    project_id integer NOT NULL,
+    user_id character varying NOT NULL,
+    "timestamp" bigint,
+    info json,
+    duplicates integer,
+    PRIMARY KEY (project_id, group_id, task_id, user_id)
+    FOREIGN KEY (project_id) REFERENCES projects (project_id)
+    FOREIGN KEY (group_id) REFERENCES groups (group_id)
+    FOREIGN KEY (task_id) REFERENCES tasks (task_id)
+    FOREIGN KEY (user_id) REFERENCES users (user_id)
 );
+
 CREATE INDEX results_taskid ON public.results USING btree (task_id);
 CREATE INDEX results_projectid ON public.results USING btree (project_id);
 CREATE INDEX results_userid ON public.results USING btree (user_id);
 
+
 CREATE TABLE IF NOT EXISTS progress(
-  project_id int
-  ,contributors int
-  ,progress int
-  ,timestamp bigint
-
-);
-
-
+    project_id int
+    contributors int
+    progress int
+    timestamp bigint
+    PRIMARY KEY (project_id, group_id, task_id, user_id)
+    FOREIGN KEY (group_id) REFERENCES groups (group_id)
+    );
