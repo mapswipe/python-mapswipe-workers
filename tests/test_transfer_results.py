6aa10bfb40110d649599ea734af29e02624ee7e2
Drew DeVault <sir@cmpwn.com>
2019-04-08 15:48:11 +0200

    improve testing

--- tests/test_transfer_results.py
+++ tests/test_transfer_results.py
@@ -1,6 +1,6 @@
 from mapswipe_workers import transfer_results
 
-transfer_results.transfer_results()
+transfer_results.run_transfer_results()
 
 print(
         'Transfered results from Firebase to Postgres' +
---
+++ tests/test_update.py
@@ -0,0 +1,6 @@
+from mapswipe_workers import update
+
+update.update_user_data()
+
+print('updated user information from Firebase in Postgres')
+
