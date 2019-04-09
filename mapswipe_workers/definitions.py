8d45558e8715dfdffa6d85c1d6b3988d8914a932
Drew DeVault <sir@cmpwn.com>
2019-04-09 11:40:54 +0200

    adhere to linux stardarts regarding config and data paths

--- mapswipe_workers/definitions.py
+++ mapswipe_workers/definitions.py
@@ -6,25 +6,32 @@ from logging.handlers import TimedRotatingFileHandler
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-# CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, '/cfg/configuration.json'))
-CONFIG_PATH = os.path.join(ROOT_DIR, 'cfg', 'configuration.json')
-# SERVICE_ACCOUNT_KEY_PATH = os.path.abspath(os.path.join(ROOT_DIR, '/cfg/serviceAccountKey.json'))
-SERVICE_ACCOUNT_KEY_PATH = os.path.join(ROOT_DIR, 'cfg', 'serviceAccountKey.json')
-DATA_PATH = os.path.join(ROOT_DIR, 'data')
-# DATA_PATH = get_data_path()
-LOGGING_CONFIG_PATH = os.path.join(ROOT_DIR, 'logging.cfg')
+# ROOT_DIR = '/python-mapswipe-workers'
+
+CONFIG_PATH = '/etc/mapswipe_workers/configuration.json'
+# CONFIG_PATH = os.path.abspath(
+#         os.path.join(ROOT_DIR, './cfg/configuration.json')
+#         )
+
+SERVICE_ACCOUNT_KEY_PATH = '/etc/mapswipe_workers/serviceAccountKey.json'
+# SERVICE_ACCOUNT_KEY_PATH = os.path.abspath(
+#         os.path.join(ROOT_DIR, 'cfg', 'serviceAccountKey.json')
+#         )
+
+DATA_PATH = '/var/lib/mapswipe_workers'
+# DATA_PATH = os.path.abspath(
+#         os.path.join(ROOT_DIR, './data/')
+#         )
+
+LOGGING_CONFIG_PATH = os.path.abspath(
+        os.path.join(ROOT_DIR, 'logging.cfg')
+        )
 
 logging.config.fileConfig(
         fname=LOGGING_CONFIG_PATH,
         disable_existing_loggers=True
         )
 logger = logging.getLogger('Mapswipe Workers')
-
-# def get_data_path():
-#     '''reads data path from config'''
-#     with open(CONFIG_PATH) as config_file:
-#         config = json.load(config_file)
-#     return config['mapswipe_workers']['data_path']
 
 
 class CustomError(Exception):
