c62f9853de10c6645d2e7df89cbf690b4b765916
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-04-15 14:54:18 +0200

    changed configuration path to /home/matthias/.config

--- mapswipe_workers/definitions.py
+++ mapswipe_workers/definitions.py
@@ -2,18 +2,19 @@ import os
 import json
 import logging
 import logging.config
+from xdg.BaseDirectory import xdg_config_home
 from logging.handlers import TimedRotatingFileHandler
 
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 # ROOT_DIR = '/python-mapswipe-workers'
 
-CONFIG_PATH = '/etc/mapswipe_workers/configuration.json'
+CONFIG_PATH = f'{xdg_config_home}/mapswipe_workers/configuration.json'
 # CONFIG_PATH = os.path.abspath(
 #         os.path.join(ROOT_DIR, './cfg/configuration.json')
 #         )
 
-SERVICE_ACCOUNT_KEY_PATH = '/etc/mapswipe_workers/serviceAccountKey.json'
+SERVICE_ACCOUNT_KEY_PATH = f'{xdg_config_home}/mapswipe_workers/serviceAccountKey.json'
 # SERVICE_ACCOUNT_KEY_PATH = os.path.abspath(
 #         os.path.join(ROOT_DIR, 'cfg', 'serviceAccountKey.json')
 #         )
