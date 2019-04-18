c402f471047b05bc0c495628df2ca0e7dc086474
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-04-18 14:01:17 +0200

    changed version number and author names. Added support for Click (cli)

--- setup.py
+++ setup.py
@@ -5,11 +5,15 @@ with open('requirements.txt') as f:
 
 setup(
     name='mapswipe-workers',
-    version='0.0.2',
+    version='3.0',
     description='Install script for the mapswipe Python workers.',
-    author='B. Herfort',
+    author='B. Herfort, M. Schaub, M. Reinmuth',
     author_email='',
     url='www.mapswipe.org',
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=requirements
+    entry_points='''
+        [console_scripts]
+        run=run:cli
+    ''',
 )
