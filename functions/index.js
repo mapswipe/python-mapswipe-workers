0ec15494d1c70dd57eb298b50c3e5dc8457022aa
Asa Schachar <asa@optimizely.com>
2019-06-21 13:59:51 -0700

    Send all user properties from firebase to segemnt

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 const functions = require('firebase-functions');
 const Analytics = require('analytics-node');
 const analytics = new Analytics(functions.config().segment.write_key);
@@ -6,8 +7,8 @@ exports.trackNewUser = functions.auth.user().onCreate((user) => {
   analytics.identify({
     userId: user.uid,
     traits: {
-      name: user.displayName,
       email: user.email,
+      ...user
     }
   });
 });
