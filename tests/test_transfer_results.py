163a86d06fa597701e74f1ef12b590acdee6b365
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-06-27 15:40:46 +0200

    run copy new users and transfer results

--- tests/test_transfer_results.py
+++ tests/test_transfer_results.py
@@ -1,6 +1,8 @@
 from mapswipe_workers.firebase_to_postgres import transfer_results
+from mapswipe_workers.firebase_to_postgres import update_data
 
 
+update_data.copy_new_users()
 transfer_results.transfer_results()
 print(
         'Transfered results from Firebase to Postgres' +
