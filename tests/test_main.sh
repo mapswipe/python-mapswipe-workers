3577adfc8ca119dc6c7ad240768e16feab8e9d40
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-06-27 15:39:30 +0200

    added test_transfer_results

--- tests/test_main.sh
+++ tests/test_main.sh
@@ -30,13 +30,13 @@ else
     exit
 fi
 
-# python test_transfer_results.py
-# if [[ $? = 0 ]]; then
-#     echo "success"
-# else
-#     echo "failure: $?"
-#     exit
-# fi
+python test_transfer_results.py
+if [[ $? = 0 ]]; then
+    echo "success"
+else
+    echo "failure: $?"
+    exit
+fi
 
 python test_terminate.py
 if [[ $? = 0 ]]; then
