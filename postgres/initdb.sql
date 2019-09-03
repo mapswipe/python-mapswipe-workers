1cb8d3174648be837b85b89adf6e53f5424ef90b
Herfort <h9b@geog>
2019-09-03 15:57:28 +0200

    implement statistics as views #120

--- postgres/initdb.sql
+++ postgres/initdb.sql
@@ -101,15 +101,91 @@ GRANT CONNECT ON DATABASE mapswipe TO backup;
 GRANT USAGE ON SCHEMA public TO backup;
 GRANT SELECT ON ALL TABLES IN SCHEMA public TO backup;
 
--- create views for aggregated statistics
+-- create views for statistics
+-- projects
+-- aggregated_projects
+CREATE or REPLACE VIEW aggregated_projects AS
+SELECT
+    count(*) as total_projects_count
+    ,Sum(CASE WHEN progress = 100  THEN 1 ELSE 0 END) as finished_projects_count
+    ,Sum(CASE WHEN status = 'active' THEN 1 ELSE 0 END) as active_projects_count
+    ,Sum(CASE WHEN status = 'inactive' THEN 1 ELSE 0 END) as inactive_projects_count
+    ,Round(Avg(contributor_count),3) as average_contributor_count
+    ,Round(Avg(progress),3) as average_progress
+    ,Round(Avg(number_of_tasks),0) as average_number_of_tasks
+FROM
+    projects;
 
+-- aggregated_projects_by_project_type
+CREATE or REPLACE VIEW aggregated_projects_by_project_type AS
+SELECT
+    project_type
+    ,count(*) as total_projects_count
+    ,Sum(CASE WHEN progress = 100  THEN 1 ELSE 0 END) as finished_projects_count
+    ,Sum(CASE WHEN status = 'active' THEN 1 ELSE 0 END) as active_projects_count
+    ,Sum(CASE WHEN status = 'inactive' THEN 1 ELSE 0 END) as inactive_projects_count
+    ,Round(Avg(contributor_count),3) as average_contributor_count
+    ,Round(Avg(progress),3) as average_progress
+    ,Round(Avg(number_of_tasks),0) as average_number_of_tasks
+FROM
+    projects
+GROUP BY
+    project_type
+ORDER BY
+    project_type;
+
+-- users
+-- aggregated_users
+CREATE or REPLACE VIEW aggregated_users AS
+SELECT
+  count(*) as total_users_count
+  ,Sum(CASE WHEN task_contribution_count > 0 THEN 1 ELSE 0 END) as active_users_count
+  ,Sum(CASE WHEN task_contribution_count = 0 THEN 1 ELSE 0 END) as inactive_users_count
+  ,round(avg(CASE WHEN task_contribution_count > 0 THEN project_contribution_count ELSE NULL END),1) as average_project_contribution_count
+  ,round(avg(CASE WHEN task_contribution_count > 0 THEN group_contribution_count ELSE NULL END),1) as average_group_contribution_count
+  ,round(avg(CASE WHEN task_contribution_count > 0 THEN task_contribution_count ELSE NULL END),1) as average_task_contribution_count
+FROM
+(
+  SELECT
+	distinct(users.user_id) as user_id
+	,count(distinct(results.project_id)) as project_contribution_count
+	,count(distinct(results.group_id)) as group_contribution_count
+	,count(distinct(results.task_id)) as task_contribution_count
+	,min(timestamp) as first_contribution_timestamp
+	,max(timestamp) as last_contribution_timestamp
+FROM
+users
+LEFT JOIN results ON
+	users.user_id = results.user_id
+GROUP BY users.user_id
+) as foo;
+
+-- results
+-- aggregated_results
 CREATE or REPLACE VIEW aggregated_results AS
 select
+	count(distinct(project_id)) as  projects_count
+	,count(distinct(group_id, project_id)) as groups_count
+	,count(distinct(task_id,group_id,project_id)) as tasks_count
+	,count(distinct(user_id)) as users_count
+	,count(*) as total_results_count
+	,sum(CASE WHEN result = 0 THEN 1 ELSE 0	END) AS "0_results_count"
+	,sum(CASE WHEN result = 1 THEN 1 ELSE 0	END) AS "1_results_count"
+	,sum(CASE WHEN result = 2 THEN 1 ELSE 0	END) AS "2_results_count"
+	,sum(CASE WHEN result = 3 THEN 1 ELSE 0	END) AS "3_results_count"
+	,round(sum(CASE WHEN result = 0 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "0_results_share"
+	,round(sum(CASE WHEN result = 1 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "1_results_share"
+	,round(sum(CASE WHEN result = 2 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "2_results_share"
+	,round(sum(CASE WHEN result = 3 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "3_results_share"
+	,min(timestamp) as first_timestamp
+	,max(timestamp) as last_timestamp
+from
+	results;
+
+-- aggregated_results_by_task_id
+CREATE or REPLACE VIEW aggregated_results_by_task_id AS
+select
     *
-    ,round("0_results_count"::numeric/total_results_count::numeric, 3) as "0_results_share"
-    ,round("1_results_count"::numeric/total_results_count::numeric, 3) as "1_results_share"
-    ,round("2_results_count"::numeric/total_results_count::numeric, 3) as "2_results_share"
-    ,round("3_results_count"::numeric/total_results_count::numeric, 3) as "3_results_share"
     ,CASE WHEN total_results_count = 1 THEN 1.0	ELSE (
       round(
           ((1.0 / (total_results_count::numeric * (total_results_count::numeric - 1.0)))
@@ -136,89 +212,111 @@ from
 		,sum(CASE WHEN result = 1 THEN 1 ELSE 0	END) AS "1_results_count"
 		,sum(CASE WHEN result = 2 THEN 1 ELSE 0	END) AS "2_results_count"
 		,sum(CASE WHEN result = 3 THEN 1 ELSE 0	END) AS "3_results_count"
+		,round(sum(CASE WHEN result = 0 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "0_results_share"
+		,round(sum(CASE WHEN result = 1 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "1_results_share"
+		,round(sum(CASE WHEN result = 2 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "2_results_share"
+		,round(sum(CASE WHEN result = 3 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "3_results_share"
+		,min(timestamp) as first_timestamp
+		,max(timestamp) as last_timestamp
 	from
 		results
 	group by
 		project_id, group_id, task_id
 ) as foo;
 
-CREATE or REPLACE VIEW aggregated_projects AS
-SELECT
-    count(*) as total_projects_count
-    ,Sum(CASE WHEN progress = 100  THEN 1 ELSE 0 END) as finished_projects_count
-    ,Sum(CASE WHEN status = 'active' THEN 1 ELSE 0 END) as active_projects_count
-    ,Sum(CASE WHEN status = 'inactive' THEN 1 ELSE 0 END) as inactive_projects_count
-    ,Round(Avg(contributor_count),3) as average_contributor_count
-    ,Round(Avg(progress),3) as average_progress
-    ,Round(Avg(number_of_tasks),0) as average_number_of_tasks
-FROM
-    projects;
-
-CREATE or REPLACE VIEW aggregated_projects_by_type AS
-SELECT
-    project_type
-    ,count(*) as total_projects_count
-    ,Sum(CASE WHEN progress = 100  THEN 1 ELSE 0 END) as finished_projects_count
-    ,Sum(CASE WHEN status = 'active' THEN 1 ELSE 0 END) as active_projects_count
-    ,Sum(CASE WHEN status = 'inactive' THEN 1 ELSE 0 END) as inactive_projects_count
-    ,Round(Avg(contributor_count),3) as average_contributor_count
-    ,Round(Avg(progress),3) as average_progress
-    ,Round(Avg(number_of_tasks),0) as average_number_of_tasks
-FROM
-    projects
+-- aggregated_results_by_project_id
+CREATE or REPLACE VIEW aggregated_results_by_project_id AS
+select
+	project_id
+	,count(distinct(group_id, project_id)) as groups_count
+	,count(distinct(task_id,group_id,project_id)) as tasks_count
+	,count(distinct(user_id)) as users_count
+	,count(*) as total_results_count
+	,sum(CASE WHEN result = 0 THEN 1 ELSE 0	END) AS "0_results_count"
+	,sum(CASE WHEN result = 1 THEN 1 ELSE 0	END) AS "1_results_count"
+	,sum(CASE WHEN result = 2 THEN 1 ELSE 0	END) AS "2_results_count"
+	,sum(CASE WHEN result = 3 THEN 1 ELSE 0	END) AS "3_results_count"
+	,round(sum(CASE WHEN result = 0 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "0_results_share"
+	,round(sum(CASE WHEN result = 1 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "1_results_share"
+	,round(sum(CASE WHEN result = 2 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "2_results_share"
+	,round(sum(CASE WHEN result = 3 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "3_results_share"
+	,min(timestamp) as first_timestamp
+	,max(timestamp) as last_timestamp
+from
+	results
 GROUP BY
-    project_type
-ORDER BY
-    project_type;
+	project_id;
 
-CREATE or REPLACE VIEW aggregated_users AS
-SELECT
-  count(*) as total_users_count
-  ,Sum(CASE WHEN task_contribution_count > 0 THEN 1 ELSE 0 END) as active_users_count
-  ,Sum(CASE WHEN task_contribution_count = 0 THEN 1 ELSE 0 END) as inactive_users_count
-  ,round(avg(CASE WHEN task_contribution_count > 0 THEN project_contribution_count ELSE NULL END),1) as average_project_contribution_count
-  ,round(avg(CASE WHEN task_contribution_count > 0 THEN group_contribution_count ELSE NULL END),1) as average_group_contribution_count
-  ,round(avg(CASE WHEN task_contribution_count > 0 THEN task_contribution_count ELSE NULL END),1) as average_task_contribution_count
-FROM
-(
-  SELECT
-	distinct(users.user_id) as user_id
-	,count(distinct(results.project_id)) as project_contribution_count
-	,count(distinct(results.group_id)) as group_contribution_count
-	,count(distinct(results.task_id)) as task_contribution_count
-	,min(timestamp) as first_contribution_timestamp
-	,max(timestamp) as last_contribution_timestamp
-FROM
-users
-LEFT JOIN results ON
-	users.user_id = results.user_id
-GROUP BY users.user_id
-) as foo;
-
-CREATE or REPLACE VIEW aggregated_results_by_user_id AS
-SELECT
-	distinct(users.user_id) as user_id
-	,users.username as user_name
-	,count(distinct(results.project_id)) as project_contribution_count
-	,count(distinct(results.group_id)) as group_contribution_count
-	,count(distinct(results.task_id)) as task_contribution_count
-	,min(timestamp) as first_contribution_timestamp
-	,max(timestamp) as last_contribution_timestamp
-FROM
-users
-LEFT JOIN results ON
-	users.user_id = results.user_id
-GROUP BY users.user_id, users.username;
-
+-- aggregated_results_by_project_id_and_date
 CREATE or REPLACE VIEW aggregated_results_by_project_id_and_date AS
-SELECT
- project_id
- ,count(*) as total_results_count
- ,count(distinct(user_id)) as total_users_count
- ,extract(year from timestamp) as year
- ,extract(month from timestamp) as month
- ,extract(day from timestamp) as day
-FROM
- results
+select
+	project_id
+	,date_trunc('day'::text, results."timestamp") AS day
+	,count(distinct(group_id, project_id)) as groups_count
+	,count(distinct(task_id,group_id,project_id)) as tasks_count
+	,count(distinct(user_id)) as users_count
+	,count(*) as total_results_count
+	,sum(CASE WHEN result = 0 THEN 1 ELSE 0	END) AS "0_results_count"
+	,sum(CASE WHEN result = 1 THEN 1 ELSE 0	END) AS "1_results_count"
+	,sum(CASE WHEN result = 2 THEN 1 ELSE 0	END) AS "2_results_count"
+	,sum(CASE WHEN result = 3 THEN 1 ELSE 0	END) AS "3_results_count"
+	,round(sum(CASE WHEN result = 0 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "0_results_share"
+	,round(sum(CASE WHEN result = 1 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "1_results_share"
+	,round(sum(CASE WHEN result = 2 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "2_results_share"
+	,round(sum(CASE WHEN result = 3 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "3_results_share"
+	,min(timestamp) as first_timestamp
+	,max(timestamp) as last_timestamp
+from
+	results
 GROUP BY
- project_id, year, month, day;
+	project_id, day;
+
+-- aggregated_results_by_user_id
+CREATE or REPLACE VIEW aggregated_results_by_user_id AS
+select
+	user_id
+	,count(distinct(project_id)) as projects_count
+	,count(distinct(group_id, project_id)) as groups_count
+	,count(distinct(task_id,group_id,project_id)) as tasks_count
+	,count(*) as total_results_count
+	,sum(CASE WHEN result = 0 THEN 1 ELSE 0	END) AS "0_results_count"
+	,sum(CASE WHEN result = 1 THEN 1 ELSE 0	END) AS "1_results_count"
+	,sum(CASE WHEN result = 2 THEN 1 ELSE 0	END) AS "2_results_count"
+	,sum(CASE WHEN result = 3 THEN 1 ELSE 0	END) AS "3_results_count"
+	,round(sum(CASE WHEN result = 0 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "0_results_share"
+	,round(sum(CASE WHEN result = 1 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "1_results_share"
+	,round(sum(CASE WHEN result = 2 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "2_results_share"
+	,round(sum(CASE WHEN result = 3 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "3_results_share"
+	,min(timestamp) as first_timestamp
+	,max(timestamp) as last_timestamp
+from
+	results
+GROUP BY
+	user_id;
+
+-- aggregated_results_by_user_id_and_date
+CREATE or REPLACE VIEW aggregated_results_by_user_id_and_date AS
+select
+	user_id
+	,date_trunc('day'::text, results."timestamp") AS day
+	,count(distinct(project_id)) as projects_count
+	,count(distinct(group_id, project_id)) as groups_count
+	,count(distinct(task_id,group_id,project_id)) as tasks_count
+	,count(*) as total_results_count
+	,sum(CASE WHEN result = 0 THEN 1 ELSE 0	END) AS "0_results_count"
+	,sum(CASE WHEN result = 1 THEN 1 ELSE 0	END) AS "1_results_count"
+	,sum(CASE WHEN result = 2 THEN 1 ELSE 0	END) AS "2_results_count"
+	,sum(CASE WHEN result = 3 THEN 1 ELSE 0	END) AS "3_results_count"
+	,round(sum(CASE WHEN result = 0 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "0_results_share"
+	,round(sum(CASE WHEN result = 1 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "1_results_share"
+	,round(sum(CASE WHEN result = 2 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "2_results_share"
+	,round(sum(CASE WHEN result = 3 THEN 1 ELSE 0	END)::numeric / count(*),3) AS "3_results_share"
+	,min(timestamp) as first_timestamp
+	,max(timestamp) as last_timestamp
+from
+	results
+GROUP BY
+	user_id, day;
+
+
+
