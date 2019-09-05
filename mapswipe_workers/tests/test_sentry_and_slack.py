6216e23dcd726a0a3a862046266335f44b78953f
h9b <Hagellach37@users.noreply.github.com>
2019-09-05 09:59:18 +0200

    set up sentry and slack #141

--- mapswipe_workers/tests/test_sentry_and_slack.py
+++ mapswipe_workers/tests/test_sentry_and_slack.py
@@ -3,6 +3,7 @@ from mapswipe_workers.utils import slack
 
 sentry.init_sentry()
 
+# this in a caught exception
 try:
     division_by_zero = 1 / 0
 except Exception as e:
@@ -10,6 +11,7 @@ except Exception as e:
     sentry.capture_exception_sentry(e)
     slack.send_error(e)
 
-
+# this a just an exeception
+division_by_zero = 1 / 0
 
 
