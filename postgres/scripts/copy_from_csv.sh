3988f9cf2d75d5f3e7f9b48db67c8edf1a561e0a
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-09-12 20:34:57 +0200

    working on postgres backup and data transfer from old instance

--- postgres/scripts/copy_from_csv.sh
+++ postgres/scripts/copy_from_csv.sh
@@ -5,9 +5,9 @@ NAME="mapswipe"
 
 for entity in projects groups tasks users results
 do
-    docker cp ${entity}.csv mapswipe_postgres:/${entity}.csv
-    docker cp copy_${entity}_from_csv.sql mapswipe_postgres:/copy_${entity}_from_csv.sql
-    docker exec -t mapswipe_postgres psql -U ${USER} -d ${NAME} -a -f /copy_${entity}_from_csv.sql
+    docker cp ${entity}.csv mapswipe_postgres:${entity}.csv
+    docker cp copy_${entity}_from_csv.sql mapswipe_postgres:copy_${entity}_from_csv.sql
+    docker exec -t mapswipe_postgres psql -U ${USER} -d ${NAME} -a -f copy_${entity}_from_csv.sql
 done
 
 
