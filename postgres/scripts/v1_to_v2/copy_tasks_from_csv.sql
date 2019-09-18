4b5100c3e119a1f0c0ac2a1450e92f77a40866ab
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-09-18 15:43:44 +0200

    bug fixing

--- postgres/scripts/v1_to_v2/copy_tasks_from_csv.sql
+++ postgres/scripts/v1_to_v2/copy_tasks_from_csv.sql
@@ -7,26 +7,52 @@ CREATE TEMP TABLE v1_tasks (
     project_id varchar,
     group_id int,
     task_id varchar,
-    geom geometry(MULTIPOLYGON,4326) DEFAULT NULL,
-    wkt varchar,
-    project_type_specifics json
+    project_type_specifics json,
+    geom geometry(MULTIPOLYGON,4326) DEFAULT NULL
 );
 
 -- Has to be in one line otherwise syntax error
 \copy v1_tasks(project_id, group_id, task_id, project_type_specifics) FROM tasks.csv WITH (FORMAT CSV, DELIMITER ',', HEADER TRUE);
 
+/* Compute geometry of tasks from x, y tile coordinates and z zoom level stored in task id*/
+/* More details on the coordinates: */
+/* https://docs.microsoft.com/en-us/bingmaps/articles/bing-maps-tile-system */
+WITH bbox as(
+    WITH lat_lon as(
+        WITH pixel_coord as(
+            /* Get zoom level and x and y as pixel coordinates from the task id */
+            /* calculate pixel coordinates from tile coordinates */
+            SELECT
+                task_id,
+                256 * pow(2, split_part(task_id, '-', 1)::int) as mapsize,
+                split_part(task_id, '-', 2)::int * 256 as x_left,
+                split_part(task_id, '-', 3)::int * 256 as y_top,
+                (split_part(task_id, '-', 2)::int + 1) * 256 as x_right,
+                (split_part(task_id, '-', 3)::int +1) * 256 as y_bottom
+            FROM v1_tasks
+        )
+        /* Compute latitude, longituted from pixel coordinates at a given zoom level */
+        SELECT
+            task_id,
+            ((x_left / mapsize) - 0.5) * 360 as lon_left,
+            90 - 360 * atan(exp(-(0.5 - (y_top / mapsize)) * 2 * pi())) / pi() as lat_top,
+            ((x_right / mapsize) - 0.5) * 360 as lon_right,
+            90 - 360 * atan(exp(-(0.5 - (y_bottom / mapsize)) * 2 * pi())) / pi() as lat_bottom
+        FROM pixel_coord
+    )
+    SELECT
+        task_id,
+        ST_SetSRID(ST_Multi(ST_Envelope(ST_MakeLine(ST_MakePoint(lon_left, lat_top), ST_MakePoint(lon_right, lat_bottom)))), 4326) as geom
+    FROM lat_lon
+)
+UPDATE
+    v1_tasks t
+SET
+    geom = b.geom
+FROM bbox b
+WHERE t.task_id = b.task_id;
 
--- Extract geometry (wkt) from project type specifics attribute (json)
-UPDATE v1_tasks t1
-SET wkt = (SELECT project_type_specifics->>'wkt');
-
--- Convert geometry to postgis geometry type.
-UPDATE v1_tasks
-SET geom = ST_Force2D(ST_Multi(ST_GeomFromText(wkt, 4326)));
-
--- Insert or update data of temp table to the permant table.
--- Note that the special excluded table is used to
--- reference values originally proposed for insertion
+/* Insert or update data of temp table to the permant table. */
 INSERT INTO
   tasks(
     project_id,
