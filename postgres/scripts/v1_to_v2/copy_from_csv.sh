66f93fec2facc093d06312ca0c65c9280aeae772
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-09-24 16:57:00 +0200

    fixed bug for when sql statements for projects gets generated

--- postgres/scripts/v1_to_v2/copy_from_csv.sh
+++ postgres/scripts/v1_to_v2/copy_from_csv.sh
@@ -2,11 +2,13 @@
 
 USER="mapswipe_workers"
 NAME="mapswipe"
-#tasks users results
-for entity in users results
+
+for entity in projects groups tasks users results
 do
     psql -h localhost -p 5432 -U ${USER} -d ${NAME} -a -f copy_${entity}_from_csv.sql
 done
+
+# Commands if docker postgres port is not exposed:
 # docker cp ${entity}.csv mapswipe_postgres:${entity}.csv
 # docker cp copy_${entity}_from_csv.sql mapswipe_postgres:copy_${entity}_from_csv.sql
 # docker exec -t mapswipe_postgres psql -U ${USER} -d ${NAME} -a -f copy_${entity}_from_csv.sql
