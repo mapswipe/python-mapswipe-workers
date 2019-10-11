2522bb62c1473e8e20d422da476409894ae51088
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-10-11 15:07:29 +0200

    user ssh tunnel to expose postgres to local pg_dump

---
+++ postgres/scripts/backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+SSH_REMOTE_HOST=username@hostname
+PGUSER="backup"
+PGDATABASE="mapswipe"
+PGPASSWORD="backupuserpassword"
+
+# Create a ssh tunnel in the background and save PID
+ssh -Cfo ExitOnForwardFailure=yes -NL 1111:localhost:5432 ${SSH_REMOTE_HOST}
+PID=$(pgrep -f 'NL 1111:')
+
+PGPASSWORD=$PGPASSWORD pg_dump \
+    --dbname=$PGDATABASE \
+    --username $PGUSER \
+    --host localhost \
+    --port 1111 \
+    --schema=public \
+    | gzip | split -b 100m - mapswipe.sql.gz
+
+kill ${PID}
