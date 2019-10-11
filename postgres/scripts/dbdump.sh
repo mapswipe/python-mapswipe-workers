ea0a53e8fd43db59ac785e4a974f7e8e33fc5a69
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-10-11 12:46:17 +0200

    access database utilizing ssh tunnel

--- postgres/scripts/dbdump.sh
+++ postgres/scripts/dbdump.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-DBUSER="backup"
-DBNAME="mapswipe"
-DBPASSWD="backupuserpassword"
+SSH_REMOTE_HOST=username@111.11.111.0
+PGUSER="backup"
+PGDATABASE="mapswipe"
+PGPASSWORD="backupuserpassword"
 
-docker exec -t postgres PGPASSWORD=$DBPASSWD pg_dump -U $DBUSER -d $DBNAME | gzip | split -b 100m - mapswipe.sql.gz
+# Create a ssh tunnel in the background and save PID
+ssh -Cfo ExitOnForwardFailure=yes -NL 1111:localhost:5432 ${SSH_REMOTE_HOST}
+PID=$(pgrep -f 'NL 1111:')
+
+PGPASSWORD=$PGPASSWORD pg_dump -d $PGDATABASE -U $PGUSER -h localhost -p 1111 | gzip | split -b 100m - mapswipe.sql.gz
+
+kill ${PID}
