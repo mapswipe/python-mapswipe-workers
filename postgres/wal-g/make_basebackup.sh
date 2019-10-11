71bf7885c995de005c63683b685732b577981572
Matthias Schaub <schaub@stud.uni-heidelberg.de>
2019-10-11 12:31:44 +0200

    Setting the right Postgres environment vars for wal-g

--- postgres/wal-g/make_basebackup.sh
+++ postgres/wal-g/make_basebackup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+export PGDATABSE=$POSTGRES_DB
+export PGUSER=$POSTGRES_USER
+export PGPASSWORD=$POSTGRES_PASSWORD
+
 wal-g backup-push /var/lib/postgresql/data
--- postgres/wal-g/restore_command.sh
+++ postgres/wal-g/restore_command.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
+export PGDATABSE=$POSTGRES_DB
+export PGUSER=$POSTGRES_USER
+export PGPASSWORD=$POSTGRES_PASSWORD
+
 wal-g wal-fetch $1 $2
