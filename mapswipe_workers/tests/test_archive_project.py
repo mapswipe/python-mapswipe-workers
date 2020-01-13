a9b29defe5b101ef603cfa392b399680f70640fa
matthias_schaub <schaubm@posteo.de>
2020-01-13 11:39:29 +0100

    use helper functions to create a project.

--- mapswipe_workers/tests/test_archive_project.py
+++ mapswipe_workers/tests/test_archive_project.py
@@ -2,25 +2,34 @@ import unittest
 
 from mapswipe_workers import auth
 from mapswipe_workers.firebase_to_postgres import archive_project
+import create_test_project
 
 
 class TestArchiveProject(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up before tests are run."""
-        cls.project_id = "test_archive_project"
-        # TODO: Create project
+        cls.project_id = create_test_project(1)
         archive_project.archive_project(cls.project_id)
 
     @classmethod
     def tearDownClass(cls):
         """Tear down after tests are run."""
         pg_db = auth.postgresDB()
-        sql_query = "".format(cls.project_id)
+        sql_query = "DELETE FROM projects WHERE project_id = {0}".format(cls.project_id)
         pg_db.query(sql_query)
 
         fb_db = auth.firebaseDB()
         ref = fb_db.reference("v2/projects/{0}".format(cls.project_id))
+        ref.set({})
+
+        ref = fb_db.reference("v2/groups/{0}".format(cls.project_id))
+        ref.set({})
+
+        ref = fb_db.reference("v2/tasks/{0}".format(cls.project_id))
+        ref.set({})
+
+        ref = fb_db.reference("v2/results/{0}".format(cls.project_id))
         ref.set({})
 
     def test_firebase_changes(self):
