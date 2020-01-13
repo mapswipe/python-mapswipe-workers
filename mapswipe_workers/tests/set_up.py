420dd3d133a17bed5b51d3bdfa7cff602ae88bbf
matthias_schaub <schaubm@posteo.de>
2020-01-13 13:30:00 +0100

    helper functions for set up and tear down of tests

--- mapswipe_workers/tests/set_up.py
+++ mapswipe_workers/tests/set_up.py
@@ -1,28 +1,47 @@
-"""Helper functions for tests"""
+"""Helper functions for test set up"""
 
 import os
 import json
 
 from mapswipe_workers import auth
-from mapswipe_workers.definitions import PROJECT_TYPE_NAMES
 
 
-# DATA_TYPE_NAME = ["drafts", "project", "groups", "tasks", "results"]
+DATA_TYPES = {
+    "draft": "projectDrafts",
+    "project": "projects",
+    "group": "groups",
+    "task": "tasks",
+    "result": "results",
+}
 
 
-def load_test_data(data_type_name: str, project_type: int) -> dict:
+def load_test_data(data_type: str, project_type: str) -> dict:
     """Load test data of given data type and project type from data directory."""
-    project_type_name = PROJECT_TYPE_NAMES[project_type]
-    project_type_name = project_type_name.lower().replace(" ", "_")
-
     test_dir = os.path.abspath(__file__)
-    data_dir = os.path.join(test_dir, "/data/")
-    file_name = project_type_name + "_" + data_type_name + ".json"
+    data_dir = os.path.join(test_dir, "/data/", project_type)
+    file_name = data_type + ".json"
     file_path = os.path.join(data_dir, file_name)
 
     with open(file_path) as test_file:
         test_data = json.load(test_file)
     return test_data
+
+
+def create_test_data(data_type: str, project_type: str) -> str:
+    """Create a test data in Firebase and Posgres and return the project id."""
+    fb_db = auth.firebaseDB()
+
+    project = load_test_data("project", project_type)
+    ref = fb_db.reference(f"/v2/projects/")
+    project_id = ref.push(project)
+
+    if data_type == "project":
+        return project_id
+
+    data = load_test_data(data_type, project_type)
+    ref = fb_db.reference(f"/v2/{0}/{1}".format(DATA_TYPES[data_type], project_id))
+    ref.set(data)
+    return project_id
 
 
 def create_project_draft(project_type: int = 0) -> list:
@@ -37,15 +56,3 @@ def create_project_draft(project_type: int = 0) -> list:
         project_draft_ids.append(ref.push(project_draft))
 
     return project_draft_ids
-
-
-def create_test_project(project_type: int) -> str:
-    """Create a test project in Firebase and return the project id."""
-    project = load_test_data("project", project_type)
-
-    fb_db = auth.firebaseDB()
-    ref = fb_db.reference(f"/v2/projects/")
-
-    project_id = ref.push(project)
-
-    return project_id
