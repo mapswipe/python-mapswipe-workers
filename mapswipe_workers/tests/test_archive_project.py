69ccb851609e8f5e71cdde867659252d50dbbc29
matthias_schaub <schaubm@posteo.de>
2020-01-13 13:31:27 +0100

    make use of set up and tear down helper functions

--- mapswipe_workers/tests/test_archive_project.py
+++ mapswipe_workers/tests/test_archive_project.py
@@ -2,35 +2,22 @@ import unittest
 
 from mapswipe_workers import auth
 from mapswipe_workers.firebase_to_postgres import archive_project
-import create_test_project
+
+import set_up
+import tear_down
 
 
 class TestArchiveProject(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         """Set up before tests are run."""
-        cls.project_id = create_test_project(1)
+        cls.project_id = set_up.create_test_data("project", "build_area")
         archive_project.archive_project(cls.project_id)
 
     @classmethod
     def tearDownClass(cls):
         """Tear down after tests are run."""
-        pg_db = auth.postgresDB()
-        sql_query = "DELETE FROM projects WHERE project_id = {0}".format(cls.project_id)
-        pg_db.query(sql_query)
-
-        fb_db = auth.firebaseDB()
-        ref = fb_db.reference("v2/projects/{0}".format(cls.project_id))
-        ref.set({})
-
-        ref = fb_db.reference("v2/groups/{0}".format(cls.project_id))
-        ref.set({})
-
-        ref = fb_db.reference("v2/tasks/{0}".format(cls.project_id))
-        ref.set({})
-
-        ref = fb_db.reference("v2/results/{0}".format(cls.project_id))
-        ref.set({})
+        tear_down.delete_test_data(cls.project_id)
 
     def test_firebase_changes(self):
         """Test if groups, tasks and results are deleted from Firebase."""
@@ -48,6 +35,7 @@ class TestArchiveProject(unittest.TestCase):
     def test_postgres_changes(self):
         """Test if postgres project is archived."""
         pg_db = auth.postgresDB()
+        # TODO archived is boolean or check status?
         sql_query = "SELECT archived FROM projects WHERE project_id = {}".format(
             self.project_id
         )
