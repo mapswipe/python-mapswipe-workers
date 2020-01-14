c1514171679ef64b6a72fbf050a7496dc25422e2
matthias_schaub <schaubm@posteo.de>
2020-01-14 10:44:19 +0100

    integrate delete_project() functionality in main archive function and improve docstrings

--- mapswipe_workers/mapswipe_workers/firebase_to_postgres/archive_project.py
+++ mapswipe_workers/mapswipe_workers/firebase_to_postgres/archive_project.py
@@ -1,8 +1,5 @@
 """
 Archive a project.
-
-Delete groups, tasks and results of a project in Firebase.
-Keep all informations in Postgres.
 """
 
 from mapswipe_workers import auth
@@ -13,11 +10,18 @@ def archive_project(project_id: str) -> None:
     """
     Archive a project.
 
-    Call a function which deletes groups, tasks and results in Firebase.
-    Set archive = true for project in Postgres.
+    Deletes groups, tasks and results from Firebase.
+    Set status = archived for project in Firebase and Postgres.
     """
     logger.info("Archive project with the id {0}".format(project_id))
-    delete_project_from_firebase(project_id)
+    logger.info(
+        "Delete results, groups and tasks of project with the id {0}".format(project_id)
+    )
+
+    fb_db = auth.firebaseDB()
+    fb_db.reference("v2/results/{0}".format(project_id)).set({})
+    fb_db.reference("v2/groups/{0}".format(project_id)).set({})
+    fb_db.reference("v2/tasks/{0}".format(project_id)).set({})
 
     fb_db = auth.firebaseDB()
     ref = fb_db.reference("v2/projects/{0}/status".format(project_id))
@@ -28,14 +32,3 @@ def archive_project(project_id: str) -> None:
         project_id
     )
     pg_db.query(sql_query)
-
-
-def delete_project_from_firebase(project_id: str) -> None:
-    """Delete results, groups and tasks of given project in Firebase."""
-    logger.info(
-        "Delete results, groups and tasks of project with the id {0}".format(project_id)
-    )
-    fb_db = auth.firebaseDB()
-    fb_db.reference("v2/results/{0}".format(project_id)).set({})
-    fb_db.reference("v2/groups/{0}".format(project_id)).set({})
-    fb_db.reference("v2/tasks/{0}".format(project_id)).set({})
