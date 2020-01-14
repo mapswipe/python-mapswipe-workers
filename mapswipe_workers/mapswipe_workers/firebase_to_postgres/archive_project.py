e6bebfecf1ccc07194f1ded33785945a53505a25
matthias_schaub <schaubm@posteo.de>
2020-01-14 10:52:16 +0100

    Support for a list of project ids as argument for archive function.

--- mapswipe_workers/mapswipe_workers/firebase_to_postgres/archive_project.py
+++ mapswipe_workers/mapswipe_workers/firebase_to_postgres/archive_project.py
@@ -6,29 +6,33 @@ from mapswipe_workers import auth
 from mapswipe_workers.definitions import logger
 
 
-def archive_project(project_id: str) -> None:
+def archive_project(project_ids: list) -> None:
     """
     Archive a project.
 
     Deletes groups, tasks and results from Firebase.
     Set status = archived for project in Firebase and Postgres.
     """
-    logger.info("Archive project with the id {0}".format(project_id))
-    logger.info(
-        "Delete results, groups and tasks of project with the id {0}".format(project_id)
-    )
+    for project_id in project_ids:
+        logger.info("Archive project with the id {0}".format(project_id))
+        logger.info(
+            "Delete results, groups and tasks of project with the id {0}".format(
+                project_id
+            )
+        )
 
-    fb_db = auth.firebaseDB()
-    fb_db.reference("v2/results/{0}".format(project_id)).set({})
-    fb_db.reference("v2/groups/{0}".format(project_id)).set({})
-    fb_db.reference("v2/tasks/{0}".format(project_id)).set({})
+        fb_db = auth.firebaseDB()
+        fb_db.reference("v2/results/{0}".format(project_id)).set({})
+        fb_db.reference("v2/groups/{0}".format(project_id)).set({})
+        fb_db.reference("v2/tasks/{0}".format(project_id)).set({})
 
-    fb_db = auth.firebaseDB()
-    ref = fb_db.reference("v2/projects/{0}/status".format(project_id))
-    ref.set({"archived"})
+        fb_db = auth.firebaseDB()
+        ref = fb_db.reference("v2/projects/{0}/status".format(project_id))
+        ref.set({"archived"})
 
-    pg_db = auth.postgresDB()
-    sql_query = "UPDATE projects SET status = 'archived' WHERE project_id = {0}".format(
-        project_id
-    )
-    pg_db.query(sql_query)
+        pg_db = auth.postgresDB()
+        sql_query = (
+            "UPDATE projects SET status = 'archived' "
+            + "WHERE project_id = {0}".format(project_id)
+        )
+        pg_db.query(sql_query)
