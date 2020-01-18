fdabeec474be7ba25833a9b9084780455d2d90eb
matthias_schaub <schaubm@posteo.de>
2020-01-18 14:23:11 +0100

    tests for update_user_data function.

--- mapswipe_workers/tests/unittests/test_update_user_data.py
+++ mapswipe_workers/tests/unittests/test_update_user_data.py
@@ -1,41 +1,42 @@
 import unittest
 
 from mapswipe_workers import auth
-from mapswipe_workers.firebase_to_postgres import transfer_results, update_data
+from mapswipe_workers.firebase_to_postgres import update_data
 
 import set_up
 import tear_down
 
 
 class TestArchiveProject(unittest.TestCase):
-    @classmethod
-    def setUpClass(cls):
-        """Set up before tests are run."""
-        cls.user_id = set_up.create_test_user()
-        update_data.update_user_data([cls.user_id])
-        transfer_results.transfer_results(project_id_list=[cls.project_id])
+    def setUp(self):
+        self.user_id = set_up.create_test_user()
 
-    @classmethod
-    def tearDownClass(cls):
-        """Tear down after tests are run."""
-        tear_down.delete_test_data(cls.project_id, cls.user_id)
+    def tearDown(self):
+        tear_down.delete_test_user(self.user_id)
 
-    def test_firebase_changes(self):
-        """Test if results are deleted from Firebase."""
-        fb_db = auth.firebaseDB()
-        ref = fb_db.reference("v2/results/{0}".format(self.project_id))
-        self.assertFalse(ref.get())
-
-    def test_postgres_changes(self):
-        """Test if results are transfered to Firebase."""
+    def test_no_users_in_postgres(self):
+        """Test update users when no users are in postgres yet."""
+        update_data.update_user_data()
         pg_db = auth.postgresDB()
-        sql_query = "SELECT * FROM results WHERE project_id = {0} and user_id = {1}".format(
-            self.project_id, self.user_id
-        )
+        sql_query = "SELECT * FROM users WHERE user_id = {0}".format(self.user_id)
         result = pg_db.retr_query(sql_query)
         self.assertIsNotNone(result)
 
-        sql_query = "SELECT * FROM users WHERE user_id = {1}".format(self.user_id)
+    def test_last_updated_users(self):
+        """Test update users when some users are in postgres."""
+        update_data.update_user_data()
+        user_id = set_up.create_test_user()
+        update_data.update_user_data()
+
+        pg_db = auth.postgresDB()
+        sql_query = "SELECT * FROM users WHERE user_id = {0}".format(user_id)
+        result = pg_db.retr_query(sql_query)
+        self.assertIsNotNone(result)
+
+    def test_with_user_ids(self):
+        update_data.update_user_data([self.user_id])
+        pg_db = auth.postgresDB()
+        sql_query = "SELECT * FROM users WHERE user_id = {0}".format(self.user_id)
         result = pg_db.retr_query(sql_query)
         self.assertIsNotNone(result)
 
