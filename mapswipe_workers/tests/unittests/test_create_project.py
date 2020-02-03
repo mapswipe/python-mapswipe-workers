c2e12ba18bc78ec01aa33baa70548d9289b4aeb0
matthias_schaub <schaubm@posteo.de>
2020-02-03 16:05:50 +0100

    test template

--- mapswipe_workers/tests/unittests/test_create_project.py
+++ mapswipe_workers/tests/unittests/test_create_project.py
@@ -2,17 +2,16 @@ import unittest
 
 import set_up
 import tear_down
-from mapswipe_workers import auth
 from mapswipe_workers import mapswipe_workers
 
 
 class TestArchiveProject(unittest.TestCase):
     def setUp(self):
-        self.project_draft_id = set_up.create_test_project_draft("build_area")
-        self.project_id = ""
+        self.project_id = set_up.create_test_project_draft("build_area")
         mapswipe_workers.run_create_projects()
 
     def tearDown(self):
-        tear_down.delete_test_project_draft(self.project_draft_id)
-        if self.project_id:
-            tear_down.delete_test_project(self.project_id)
+        tear_down.delete_test_project(self.project_id)
+
+    def test_test(self):
+        print(self.project_id)
