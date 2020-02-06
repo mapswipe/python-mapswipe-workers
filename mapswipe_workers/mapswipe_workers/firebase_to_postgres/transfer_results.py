e7dae3f3b8f7b64cd7101b11439e43f96236d632
matthias_schaub <schaubm@posteo.de>
2020-02-06 14:40:53 +0100

    Run Black and flake8. Support type hints. Simplify code.

--- mapswipe_workers/mapswipe_workers/firebase_to_postgres/transfer_results.py
+++ mapswipe_workers/mapswipe_workers/firebase_to_postgres/transfer_results.py
@@ -1,5 +1,5 @@
 import csv
-import io
+from io import StringIO
 
 import dateutil.parser
 from mapswipe_workers import auth
@@ -13,7 +13,7 @@ def transfer_results(project_ids: list = None) -> list:
     # Firebase transaction function
     def transfer(current_results):
         if current_results is None:
-            logger.info(f'{project_id}: No results in Firebase')
+            logger.info(f"{project_id}: No results in Firebase")
         else:
             results_user_id_list = get_user_ids_from_results(current_results)
             update_data.update_user_data(results_user_id_list)
@@ -24,10 +24,10 @@ def transfer_results(project_ids: list = None) -> list:
 
     if not project_ids:
         # get project_ids from existing results if no project ids specified
-        project_ids = list(fb_db.reference('v2/results/').get(shallow=True))
+        project_ids = list(fb_db.reference("v2/results/").get(shallow=True))
 
     if not project_ids:
-        logger.info(f'There are no results to transfer.')
+        logger.info(f"There are no results to transfer.")
         return []
 
     # get all project ids from postgres, we will only transfer results for projects we have there
@@ -35,72 +35,69 @@ def transfer_results(project_ids: list = None) -> list:
 
     for project_id in project_ids:
         if project_id not in postgres_project_ids:
-            logger.info(f'{project_id}: This project is not in postgres. We will not transfer results')
+            logger.info(
+                f"{project_id}: This project is not in postgres. We will not transfer results"
+            )
             continue
-        elif 'tutorial' in project_id:
-            logger.info(f'{project_id}: these are results for a tutorial. we will not transfer these')
+        elif "tutorial" in project_id:
+            logger.info(
+                f"{project_id}: these are results for a tutorial. we will not transfer these"
+            )
             continue
 
-        logger.info(f'{project_id}: Start transfering results')
+        logger.info(f"{project_id}: Start transfering results")
 
-        results_ref = fb_db.reference(f'v2/results/{project_id}')
+        results_ref = fb_db.reference(f"v2/results/{project_id}")
         truncate_temp_results()
 
         try:
             results_ref.transaction(transfer)
         except fb_db.TransactionError:
             logger.exception(
-                f'{project_id}: Firebase transaction for transfering results failed to commit'
+                f"{project_id}: Firebase transaction for transfering results failed to commit"
             )
             sentry.capture_exception()
             return []
 
-        logger.info(f'{project_id}: Transfered results to postgres')
+        logger.info(f"{project_id}: Transfered results to postgres")
 
     return project_ids
 
 
-def results_to_file(results, projectId):
-    '''
+def results_to_file(results: dict, project_id: str) -> StringIO:
+    """
     Writes results to an in-memory file like object
     formatted as a csv using the buffer module (StringIO).
     This can be then used by the COPY statement of Postgres
     for a more efficient import of many results into the Postgres
     instance.
-    Parameters
-    ----------
-    results: dict
-        The results as retrived from the Firebase Realtime Database instance.
+
     Returns
     -------
     results_file: io.StingIO
         The results in an StringIO buffer.
-    '''
+    """
     # If csv file is a file object, it should be opened with newline=''
 
-    results_file = io.StringIO('')
+    results_file = StringIO("")
 
-    w = csv.writer(
-            results_file,
-            delimiter='\t',
-            quotechar="'"
-            )
+    w = csv.writer(results_file, delimiter="\t", quotechar="'")
 
-    logger.info(f'Got %s groups for project {projectId} to transfer' % len(results.items()))
+    logger.info(
+        f"Got %s groups for project {project_id} to transfer" % len(results.items())
+    )
     for groupId, users in results.items():
         for userId, results in users.items():
 
             # check if all attributes are set, if not don't transfer the results for this group
             try:
-                timestamp = results['timestamp']
-                start_time = results['startTime']
-                end_time = results['endTime']
-                results = results['results']
-            except KeyError as e:
-                sentry.capture_exception_sentry(e)
-                sentry.capture_message_sentry(f'at least one missing attribute for: {projectId}/{groupId}/{userId}, will skip this one')
-                logger.exception(e)
-                logger.warning(f'at least one missing attribute for: {projectId}/{groupId}/{userId}, will skip this one')
+                timestamp = results["timestamp"]
+                start_time = results["startTime"]
+                end_time = results["endTime"]
+                results = results["results"]
+            except KeyError:
+                logger.exception()
+                sentry.capture_exception()
                 continue
 
             timestamp = dateutil.parser.parse(timestamp)
@@ -109,16 +106,18 @@ def results_to_file(results, projectId):
 
             if type(results) is dict:
                 for taskId, result in results.items():
-                    w.writerow([
-                        projectId,
-                        groupId,
-                        userId,
-                        taskId,
-                        timestamp,
-                        start_time,
-                        end_time,
-                        result,
-                        ])
+                    w.writerow(
+                        [
+                            project_id,
+                            groupId,
+                            userId,
+                            taskId,
+                            timestamp,
+                            start_time,
+                            end_time,
+                            result,
+                        ]
+                    )
             elif type(results) is list:
                 # TODO: optimize for performance
                 # (make sure data from firebase is always a dict)
@@ -129,16 +128,18 @@ def results_to_file(results, projectId):
                     if result is None:
                         continue
                     else:
-                        w.writerow([
-                            projectId,
-                            groupId,
-                            userId,
-                            taskId,
-                            timestamp,
-                            start_time,
-                            end_time,
-                            result,
-                            ])
+                        w.writerow(
+                            [
+                                project_id,
+                                groupId,
+                                userId,
+                                taskId,
+                                timestamp,
+                                start_time,
+                                end_time,
+                                result,
+                            ]
+                        )
             else:
                 raise TypeError
 
@@ -146,55 +147,47 @@ def results_to_file(results, projectId):
     return results_file
 
 
-def save_results_to_postgres(results_file):
-    '''
+def save_results_to_postgres(results_file: StringIO) -> None:
+    """
     Saves results to a temporary table in postgres using the COPY Statement of Postgres
     for a more efficient import into the database.
-
-    Parameters
-    ----------
-    results_file: io.StringIO
-    '''
+    """
 
     p_con = auth.postgresDB()
     columns = [
-            'project_id',
-            'group_id',
-            'user_id',
-            'task_id',
-            'timestamp',
-            'start_time',
-            'end_time',
-            'result',
-            ]
-    p_con.copy_from(results_file, 'results_temp', columns)
+        "project_id",
+        "group_id",
+        "user_id",
+        "task_id",
+        "timestamp",
+        "start_time",
+        "end_time",
+        "result",
+    ]
+    p_con.copy_from(results_file, "results_temp", columns)
     results_file.close()
 
-    query_insert_results = '''
+    query_insert_results = """
         INSERT INTO results
             SELECT * FROM results_temp
         ON CONFLICT (project_id,group_id,user_id,task_id)
         DO NOTHING;
-    '''
+    """
     p_con.query(query_insert_results)
-    del(p_con)
 
 
-def truncate_temp_results():
+def truncate_temp_results() -> None:
     p_con = auth.postgresDB()
-    query_truncate_temp_results = '''
+    query_truncate_temp_results = """
                     TRUNCATE results_temp
-                '''
+                """
     p_con.query(query_truncate_temp_results)
-    del p_con
-
-    return
 
 
-def get_user_ids_from_results(results):
-    '''
+def get_user_ids_from_results(results) -> list:
+    """
     Get all users based on the ids provided in the results
-    '''
+    """
 
     user_ids = set([])
     for groupId, users in results.items():
@@ -204,17 +197,15 @@ def get_user_ids_from_results(results):
     return user_ids
 
 
-def get_projects_from_postgres():
-    '''
+def get_projects_from_postgres() -> list:
+    """
     Get the id of all projects in postgres
-    '''
+    """
 
     pg_db = auth.postgresDB()
-    sql_query = '''
+    sql_query = """
         SELECT project_id from projects;
-    '''
+    """
     raw_ids = pg_db.retr_query(sql_query, None)
     project_ids = [i[0] for i in raw_ids]
-
-    del pg_db
     return project_ids
