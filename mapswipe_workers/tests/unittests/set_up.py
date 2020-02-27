595ef8f732da7e5488d1461374b51bef0ec92b38
matthias_schaub <schaubm@posteo.de>
2020-02-27 11:21:13 +0100

    Remove unused string formatters.

--- mapswipe_workers/tests/unittests/set_up.py
+++ mapswipe_workers/tests/unittests/set_up.py
@@ -12,7 +12,7 @@ DATA_TYPES = {
 }
 
 
-def load_test_data(data_type: str, project_type: str = None) -> dict:
+def load_test_data(data_type: str, project_type: str = "") -> dict:
     """Load test data of given data type and project type from data directory."""
     test_dir = os.path.dirname(__file__)
     data_dir = os.path.join(test_dir, "data", project_type)
@@ -29,12 +29,12 @@ def create_test_project(project_type: str) -> str:
     project = load_test_data("project", project_type)
 
     fb_db = auth.firebaseDB()
-    ref = fb_db.reference(f"/v2/projects/")
-    project_id = ref.push(project)
+    ref = fb_db.reference("/v2/projects/")
+    project_id = ref.push(project).key
 
     for data_type in DATA_TYPES.keys():
         data = load_test_data(data_type, project_type)
-        ref = fb_db.reference(f"/v2/{0}/{1}".format(data_type, project_id))
+        ref = fb_db.reference("/v2/{0}/{1}".format(data_type, project_id))
         ref.set(data)
 
     # TODO: Create test project also in Postgres
@@ -46,8 +46,8 @@ def create_test_user() -> str:
     user = load_test_data("user")
 
     fb_db = auth.firebaseDB()
-    ref = fb_db.reference(f"/v2/users/")
-    user_id = ref.push(user)
+    ref = fb_db.reference("/v2/users/")
+    user_id = ref.push(user).key
 
     return user_id
 
@@ -57,7 +57,7 @@ def create_test_project_draft(project_type: str) -> list:
     project_draft = load_test_data("project_draft", project_type)
 
     fb_db = auth.firebaseDB()
-    ref = fb_db.reference(f"/v2/projectDrafts/")
+    ref = fb_db.reference("/v2/projectDrafts/")
     project_id = ref.push(project_draft).key
 
     return project_id
