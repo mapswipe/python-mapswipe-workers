81786e734e571ed37ec19f58e33beef4d9598f2a
matthias_schaub <schaubm@posteo.de>
2020-02-27 10:43:36 +0100

    Use click test runner for testing cli.

--- mapswipe_workers/tests/unittests/test_create_project.py
+++ mapswipe_workers/tests/unittests/test_create_project.py
@@ -2,19 +2,20 @@ import unittest
 
 import set_up
 import tear_down
+from click.testing import CliRunner
 from mapswipe_workers import mapswipe_workers
 
 
 class TestArchiveProject(unittest.TestCase):
     def setUp(self):
         self.project_id = set_up.create_test_project_draft("build_area")
-        mapswipe_workers.run_create_projects()
 
     def tearDown(self):
         tear_down.delete_test_project(self.project_id)
 
     def test_test(self):
-        print(self.project_id)
+        runner = CliRunner()
+        runner.invoke(mapswipe_workers.run_create_projects)
 
 
 if __name__ == "__main__":
