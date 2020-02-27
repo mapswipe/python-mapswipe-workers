c33e2d950fce104c2c05552622a534fdd97bddab
matthias_schaub <schaubm@posteo.de>
2020-02-27 11:22:25 +0100

    Rename class and function.

--- mapswipe_workers/tests/unittests/test_create_project.py
+++ mapswipe_workers/tests/unittests/test_create_project.py
@@ -3,19 +3,33 @@ import unittest
 import set_up
 import tear_down
 from click.testing import CliRunner
-from mapswipe_workers import mapswipe_workers
+from mapswipe_workers import auth, mapswipe_workers
 
 
-class TestArchiveProject(unittest.TestCase):
+class TestCreateProject(unittest.TestCase):
     def setUp(self):
         self.project_id = set_up.create_test_project_draft("build_area")
 
     def tearDown(self):
         tear_down.delete_test_project(self.project_id)
 
-    def test_test(self):
+    def test_create_project(self):
         runner = CliRunner()
         runner.invoke(mapswipe_workers.run_create_projects)
+
+        pg_db = auth.postgresDB()
+        query = "SELECT * FROM projects WHERE project_id = '{0}'".format(
+            self.project_id
+        )
+        result = pg_db.retr_query(query)
+        # TODO: Make comparision
+        print(result)
+
+        fb_db = auth.firebaseDB()
+        ref = fb_db.reference("/v2/projects/{0}".format(self.project_id))
+        result = ref.get()
+        # TODO: Make comparision
+        print(result)
 
 
 if __name__ == "__main__":
