b45a2eda8a4f847fa46cc82e90060d8b25e28db6
matthias_schaub <schaubm@posteo.de>
2020-03-04 15:25:45 +0100

    Change configuration handling to using environment variables.

--- mapswipe_workers/mapswipe_workers/utils/user_management.py
+++ mapswipe_workers/mapswipe_workers/utils/user_management.py
@@ -11,7 +11,6 @@ from requests.exceptions import HTTPError
 
 
 def set_project_manager_rights(email):
-    fb_db = firebaseDB()
     try:
         user = auth.get_user_by_email(email)
         auth.set_custom_user_claims(user.uid, {"projectManager": True})
@@ -24,7 +23,6 @@ def set_project_manager_rights(email):
 
 
 def remove_project_manager_rights(email):
-    fb_db = firebaseDB()
     try:
         user = auth.get_user_by_email(email)
         auth.update_user(user.uid, custom_claims=auth.DELETE_ATTRIBUTE)
