312f7dbc5bff33ffbb35296382ce1c3f57ce8c75
h9b <Hagellach37@users.noreply.github.com>
2020-03-10 13:29:53 +0100

    use save sql string formatting #311

--- mapswipe_workers/mapswipe_workers/firebase_to_postgres/archive_project.py
+++ mapswipe_workers/mapswipe_workers/firebase_to_postgres/archive_project.py
@@ -14,14 +14,14 @@ def archive_project(project_ids: list) -> None:
     Set status = archived for project in Firebase and Postgres.
     """
     for project_id in project_ids:
-        logger.info("Archive project with the id {0}".format(project_id))
-        logger.info("Delete results of project with the id {0}".format(project_id))
+        logger.info(f"Archive project with the id {project_id}")
+        logger.info(f"Delete results of project with the id {project_id}")
 
         fb_db = auth.firebaseDB()
-        fb_db.reference("v2/results/{0}".format(project_id)).set({})
+        fb_db.reference(f"v2/results/{project_id}").set({})
 
         # get group keys for this project to estimate size in firebase
-        groups = fb_db.reference("v2/groups/{0}/".format(project_id)).get(shallow=True)
+        groups = fb_db.reference(f"v2/groups/{project_id}").get(shallow=True)
 
         if not groups:
             logger.info("no groups to delete in firebase")
@@ -33,36 +33,28 @@ def archive_project(project_ids: list) -> None:
             # delete groups, tasks in firebase for each chunk using the update function
             for i in range(0, chunks):
                 logger.info(
-                    "Delete max {1} groups and tasks of project with the id {0}".format(
-                        project_id, chunk_size
-                    )
+                    f"Delete max {chunk_size} groups and tasks"
+                    f"of project with the id {project_id}"
                 )
                 update_dict = {}
                 for group_id in group_keys[:chunk_size]:
                     update_dict[group_id] = None
-                fb_db.reference("v2/groups/{0}".format(project_id)).update(update_dict)
-                fb_db.reference("v2/tasks/{0}".format(project_id)).update(update_dict)
+                fb_db.reference(f"v2/groups/{project_id}").update(update_dict)
+                fb_db.reference(f"v2/tasks/{project_id}").update(update_dict)
                 group_keys = group_keys[chunk_size:]
 
         logger.info(
-            "Set status=archived in Firebase for project with the id {0}".format(
-                project_id
-            )
+            f"Set status=archived in Firebase for project with the id {project_id}"
         )
         fb_db = auth.firebaseDB()
-        ref = fb_db.reference("v2/projects/{0}/status".format(project_id))
-        ref.set("archived")
+        fb_db.reference(f"v2/projects/{project_id}/status").set("archived")
 
         logger.info(
-            "Set status=archived in Postgres for project with the id {0}".format(
-                project_id
-            )
+            f"Set status=archived in Postgres for project with the id {project_id}"
         )
         pg_db = auth.postgresDB()
         sql_query = """
             UPDATE projects SET status = 'archived'
-            WHERE project_id = '{0}';
-        """.format(
-            project_id
-        )
-        pg_db.query(sql_query)
+            WHERE project_id = %(project_id)s;
+        """
+        pg_db.query(sql_query, {"project_id": project_id})
