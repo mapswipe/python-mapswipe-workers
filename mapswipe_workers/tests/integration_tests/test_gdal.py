4978b3703286148b87b5a38054aee0976fcb7e36
matthias_schaub <schaubm@posteo.de>
2020-03-23 09:45:38 +0100

    Test if ogr osr and gdal is installed.

--- mapswipe_workers/tests/integration_tests/test_gdal.py
+++ mapswipe_workers/tests/integration_tests/test_gdal.py
@@ -4,11 +4,12 @@ import unittest
 class TestArchiveProject(unittest.TestCase):
     def test_gdal_installation(self):
         try:
-            from osgeo import gdal
-
-            self.assertIsNotNone(gdal)
+            from osgeo import ogr, osr, gdal
         except ModuleNotFoundError:
-            self.assertIsNotNone(gdal)
+            pass
+        self.assertIsNotNone(ogr)
+        self.assertIsNotNone(osr)
+        self.assertIsNotNone(gdal)
 
 
 if __name__ == "__main__":
