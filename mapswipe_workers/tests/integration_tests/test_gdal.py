96da159f3de8c2f8b0da9c2e5a10863c45d3a567
matthias_schaub <schaubm@posteo.de>
2020-03-23 10:30:43 +0100

    Use TryExcept and self.raise to test import if osgeo.

--- mapswipe_workers/tests/integration_tests/test_gdal.py
+++ mapswipe_workers/tests/integration_tests/test_gdal.py
@@ -2,14 +2,11 @@ import unittest
 
 
 class TestArchiveProject(unittest.TestCase):
-    def test_gdal_installation(self):
+    def test_osgeo_installation(self):
         try:
             from osgeo import ogr, osr, gdal
         except ModuleNotFoundError:
-            pass
-        self.assertIsNotNone(ogr)
-        self.assertIsNotNone(osr)
-        self.assertIsNotNone(gdal)
+            self.fail()
 
 
 if __name__ == "__main__":
