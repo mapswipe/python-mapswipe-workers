be08c200ddd3e9b5b1fad116794807cefd4eb1c4
matthias_schaub <schaubm@posteo.de>
2020-03-24 10:51:29 +0100

    Use same logic and structure for handling of max write limit during fb interaction as delete_project.

--- mapswipe_workers/mapswipe_workers/firebase_to_postgres/archive_project.py
+++ mapswipe_workers/mapswipe_workers/firebase_to_postgres/archive_project.py
@@ -2,8 +2,16 @@
 Archive a project.
 """
 
+from firebase_admin import exceptions
+
 from mapswipe_workers import auth
 from mapswipe_workers.definitions import logger
+
+
+def chunks(data, size=250):
+    """Yield successive n-sized chunks from list."""
+    for i in range(0, len(data), size):
+        yield data[i : i + size]
 
 
 def archive_project(project_ids: list) -> None:
@@ -15,46 +23,38 @@ def archive_project(project_ids: list) -> None:
     """
     for project_id in project_ids:
         logger.info(f"Archive project with the id {project_id}")
-        logger.info(f"Delete results of project with the id {project_id}")
 
         fb_db = auth.firebaseDB()
-        fb_db.reference(f"v2/results/{project_id}").set({})
+        ref = fb_db.reference(f"v2/results/{project_id}")
+        try:
+            ref.delete()
+        except exceptions.InvalidArgumentError:
+            # Data to write exceeds the maximum size that can be modified
+            # with a single request. Delete chunks of data instead.
+            childs = ref.get(shallow=True)
+            for chunk in chunks(list(childs.keys())):
+                ref.update({key: None for key in chunk})
+            ref.delete()
 
-        # get group keys for this project to estimate size in firebase
-        groups = fb_db.reference(f"v2/groups/{project_id}").get(shallow=True)
+        ref = fb_db.reference(f"v2/results/{project_id}")
+        try:
+            ref.delete()
+        except exceptions.InvalidArgumentError:
+            # Data to write exceeds the maximum size that can be modified
+            # with a single request. Delete chunks of data instead.
+            childs = ref.get(shallow=True)
+            for chunk in chunks(list(childs.keys())):
+                ref.update({key: None for key in chunk})
+            ref.delete()
 
-        if not groups:
-            logger.info("no groups to delete in firebase")
-        else:
-            group_keys = list(groups.keys())
-            chunk_size = 250
-            chunks = int(len(group_keys) / chunk_size) + 1
-
-            # delete groups, tasks in firebase for each chunk using the update function
-            for i in range(0, chunks):
-                logger.info(
-                    f"Delete max {chunk_size} groups and tasks"
-                    f"of project with the id {project_id}"
-                )
-                update_dict = {}
-                for group_id in group_keys[:chunk_size]:
-                    update_dict[group_id] = None
-                fb_db.reference(f"v2/groups/{project_id}").update(update_dict)
-                fb_db.reference(f"v2/tasks/{project_id}").update(update_dict)
-                group_keys = group_keys[chunk_size:]
-
-        logger.info(
-            f"Set status=archived in Firebase for project with the id {project_id}"
-        )
-        fb_db = auth.firebaseDB()
+        fb_db.reference(f"v2/groups/{project_id}").delete()
         fb_db.reference(f"v2/projects/{project_id}/status").set("archived")
 
-        logger.info(
-            f"Set status=archived in Postgres for project with the id {project_id}"
-        )
         pg_db = auth.postgresDB()
         sql_query = """
             UPDATE projects SET status = 'archived'
             WHERE project_id = %(project_id)s;
         """
         pg_db.query(sql_query, {"project_id": project_id})
+
+    return True
