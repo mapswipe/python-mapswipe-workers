7d925d0c11c1bf992a011003427847dda2bb8b38
matthias_schaub <schaubm@posteo.de>
2020-04-02 12:13:23 +0200

    Use logging.cfg as config

--- mapswipe_workers/mapswipe_workers/definitions.py
+++ mapswipe_workers/mapswipe_workers/definitions.py
@@ -4,6 +4,7 @@ import os
 
 import sentry_sdk
 from xdg import XDG_DATA_HOME
+
 from mapswipe_workers.config import SENTRY_DSN
 
 
@@ -48,48 +49,17 @@ IMAGE_URL = {
 }
 
 DATA_PATH = os.path.join(XDG_DATA_HOME, "mapswipe_workers")
+if not os.path.exists(DATA_PATH):
+    os.makedirs(DATA_PATH)
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+LOGGING_CONFIG_PATH = os.path.join(ROOT_DIR, "logging.cfg")
 LOGGING_FILE_PATH = os.path.join(DATA_PATH, "mapswipe_workers.log")
 
-LOGGING_CONFIG = {
-    "version": 1,
-    "disable_existing_loggers": True,
-    "formatters": {
-        "mapswipeFormatter": {
-            "format": (
-                "%(asctime)s - %(levelname)s -",
-                "%(filename)s - %(funcName)s - %(message)s",
-            )
-        }
-    },
-    "handlers": {
-        "consoleHandler": {
-            "class": "logging.StreamHandler",
-            "level": "INFO",
-            "formatter": "mapswipeFormatter",
-            "stream": "ext:://sys.stdout",
-        },
-        "fileHandler": {
-            "class": "logging.handlers.TimedRotatingFileHandler",
-            "level": "INFO",
-            "formatter": "mapswipeFormatter",
-            "filename": LOGGING_FILE_PATH,
-            "when": "D",
-            "interval": 1,
-            "backupCount": 14,
-        },
-    },
-    "loggers": {
-        "root": {"handlers": ["consoleHandler"], "level": "INFO"},
-        "mapswipeLogger": {
-            "handlers": ["consoleHandler", "fileHandler"],
-            "level": "INFO",
-            "qualname": "mapswipeLogger",
-            "propagate": False,
-        },
-    },
-}
-
-logging.config.dictConfig(LOGGING_CONFIG)
+logging.config.fileConfig(
+    fname=LOGGING_CONFIG_PATH,
+    defaults={"logfilename": LOGGING_FILE_PATH},
+    disable_existing_loggers=True,
+)
 logger = logging.getLogger("Mapswipe Workers")
 
 try:
