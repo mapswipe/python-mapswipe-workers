d8671c2f378984d78d488cf3b41acaf32f672182
h9b <Hagellach37@users.noreply.github.com>
2020-05-26 12:10:32 +0200

    clean up and adjust tasks_to_geojson for multiple zoom levels

--- mapswipe_workers/python_scripts/tasks_to_geojson.py
+++ mapswipe_workers/python_scripts/tasks_to_geojson.py
@@ -2,17 +2,14 @@ from osgeo import ogr
 import os
 import sys
 from mapswipe_workers.utils import tile_grouping_functions as t
-from mapswipe_workers.project_types.build_area.build_area_group import BuildAreaGroup
-from mapswipe_workers.project_types.build_area.build_area_task import BuildAreaTask
-from mapswipe_workers.project_types.build_area.build_area_project import (
-    BuildAreaProject,
-)
-
+from mapswipe_workers.project_types.tile_map_service_grid.group import Group
+from mapswipe_workers.project_types.tile_map_service_grid.task import Task
+from mapswipe_workers.project_types.tile_map_service_grid.project import Project
 from mapswipe_workers.definitions import logger
-from mapswipe_workers.definitions import IMAGE_URLS
+from mapswipe_workers.project_types.base.tile_server import BaseTileServer
 
 
-def tasks_to_geojson(project_extent, zoomlevel, outfile):
+def tasks_to_geojson(project_extent_file, zoomlevel, outfile):
     """
         The function to create a geojson file from the tasks.
 
@@ -30,27 +27,17 @@ def tasks_to_geojson(project_extent, zoomlevel, outfile):
                 True if successful, False otherwise.
     """
     # select a geojson file and pass it as input parameter
-    url = IMAGE_URLS["bing"]
-
-    apiKey = ""
-
     tile_server_dict = {
         "name": "bing",
-        "url": url,
-        "apiKey": apiKey,
-        "wmtsLayerName": None,
-        "captions": None,
-        "date": None,
-        "credits": "credits",
     }
 
-    project = BuildAreaProject
+    project = Project
     project.project_type = 1
-    project.projectId = 12
+    project.projectId = "tasks_to_geojson"
     project.zoomLevel = int(zoomlevel)
-    project.tileServer = tile_server_dict
+    project.tileServer = vars(BaseTileServer(tile_server_dict))
 
-    raw_groups = t.extent_to_slices(project_extent, 18, 120)
+    raw_groups = t.extent_to_slices(project_extent_file, 18, 120)
 
     tasks = list()
 
@@ -59,11 +46,11 @@ def tasks_to_geojson(project_extent, zoomlevel, outfile):
         xMin = slice["xMin"]
         yMax = slice["yMax"]
         yMin = slice["yMin"]
-        group = BuildAreaGroup(project, group_id, slice)
+        group = Group(project, group_id, slice)
         # following lines from create_task function
         for TileX in range(int(xMin), int(xMax) + 1):
             for TileY in range(int(yMin), int(yMax) + 1):
-                task = BuildAreaTask(group, project, TileX, TileY)
+                task = Task(group, project, TileX, TileY)
                 tasks.append(vars(task))
 
     # Create the output Driver
@@ -79,11 +66,15 @@ def tasks_to_geojson(project_extent, zoomlevel, outfile):
 
     outLayer.CreateField(ogr.FieldDefn("groupId", ogr.OFTString))
     outLayer.CreateField(ogr.FieldDefn("taskId", ogr.OFTString))
+    outLayer.CreateField(ogr.FieldDefn("tile_x", ogr.OFTInteger))
+    outLayer.CreateField(ogr.FieldDefn("tile_y", ogr.OFTInteger))
+    outLayer.CreateField(ogr.FieldDefn("tile_z", ogr.OFTInteger))
 
     for task in tasks:
         geometry = task["geometry"]
         group_id = task["groupId"]
         task_id = task["taskId"]
+        tile_z, tile_x, tile_y = task["taskId"].split("-")
         geom = ogr.CreateGeometryFromWkt(geometry)
 
         featureDefn = outLayer.GetLayerDefn()
@@ -91,6 +82,9 @@ def tasks_to_geojson(project_extent, zoomlevel, outfile):
         outFeature.SetGeometry(geom)
         outFeature.SetField("groupId", group_id)
         outFeature.SetField("taskId", task_id)
+        outFeature.SetField("tile_x", int(tile_x))
+        outFeature.SetField("tile_y", (tile_y))
+        outFeature.SetField("tile_z", (tile_z))
         outLayer.CreateFeature(outFeature)
         outFeature = None
 
@@ -101,5 +95,7 @@ def tasks_to_geojson(project_extent, zoomlevel, outfile):
 
 
 if __name__ == "__main__":
-    project_extent = sys.argv[1]
-    print(tasks_to_geojson(project_extent, 18, "tasks.geojson"))
+    project_extent_file = sys.argv[1]  # geojson file
+    zoomlevel = sys.argv[2]  # zoomlevel as integer
+    output_file = project_extent_file.replace(".geojson", "_tasks.geojson")
+    print(tasks_to_geojson(project_extent_file, zoomlevel, output_file))
