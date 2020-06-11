0d607ea40ca544aa97e549984fd498a101edba43
matthias_schaub <schaubm@posteo.de>
2020-06-11 13:51:45 +0200

    Test for gdal version.

--- mapswipe_workers/tests/integration/test_gdal.py
+++ mapswipe_workers/tests/integration/test_gdal.py
@@ -6,7 +6,14 @@ class TestArchiveProject(unittest.TestCase):
         try:
             from osgeo import ogr, osr, gdal  # noqa: F401
         except ModuleNotFoundError:
-            self.fail()
+            self.fail("GDAL is not installed")
+
+    def test_osgeo_version(self):
+        from osgeo import ogr, osr, gdal  # noqa: F401
+
+        version_num = int(gdal.VersionInfo("VERSION_NUM"))
+        if version_num < 3000000:
+            self.fail("GDAL version is not >= 3.0")
 
 
 if __name__ == "__main__":
