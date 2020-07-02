5bbc06f08e6aa3d99c04e3201a8a3b4fb36660d1
h9b <Hagellach37@users.noreply.github.com>
2020-07-02 16:08:14 +0200

    add test for polygons within

--- mapswipe_workers/tests/unittests/test_groups_overlap.py
+++ mapswipe_workers/tests/unittests/test_groups_overlap.py
@@ -6,12 +6,14 @@ from mapswipe_workers.utils import tile_grouping_functions as t
 class TestGroupsOverlap(unittest.TestCase):
     def setUp(self):
         self.test_dir = os.path.dirname(os.path.abspath(__file__))
-        self.project_extent_file = os.path.join(
+
+    def test_project_geometries_intersection(self):
+
+        project_extent_file = os.path.join(
             self.test_dir, "fixtures/completeness/closed_polygons.geojson"
         )
 
-    def test_adjust_groups_overlap(self):
-        groups_with_overlaps = t.extent_to_slices(self.project_extent_file, 18, 100)
+        groups_with_overlaps = t.extent_to_slices(project_extent_file, 18, 100)
         t.save_vertical_slices_as_geojson(
             groups_with_overlaps, "groups_with_overlaps.geojson"
         )
@@ -23,6 +25,24 @@ class TestGroupsOverlap(unittest.TestCase):
 
         # we expect 117 groups
         self.assertEqual(len(groups), 117)
+
+    def test_project_geometries_within(self):
+        project_extent_file = os.path.join(
+            self.test_dir, "fixtures/completeness/project_geometries_within.geojson"
+        )
+
+        groups_with_overlaps = t.extent_to_slices(project_extent_file, 18, 100)
+        t.save_vertical_slices_as_geojson(
+            groups_with_overlaps, "groups_with_overlaps.geojson"
+        )
+
+        groups = t.adjust_overlapping_groups(groups_with_overlaps)
+
+        # save files for visual inspection in qgis
+        t.save_vertical_slices_as_geojson(groups, "groups.geojson")
+
+        # we expect 117 groups
+        self.assertEqual(len(groups), 64)
 
 
 if __name__ == "__main__":
