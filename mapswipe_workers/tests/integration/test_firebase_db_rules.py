ee0b1bebe54ceed08e5c4f18926766e654bd7098
h9b <Hagellach37@users.noreply.github.com>
2020-07-14 15:51:34 +0200

    start firebase db rules test

---
+++ mapswipe_workers/tests/integration/test_firebase_db_rules.py
@@ -0,0 +1,44 @@
+import unittest
+
+# import re
+
+# from mapswipe_workers.auth import firebaseDB
+# from mapswipe_workers.definitions import CustomError
+
+
+class TestFirebaseDBRules(unittest.TestCase):
+    def setUp(self):
+        # set up 4 users with specified roles
+        # normal app user
+
+        # team member
+
+        # project manager
+
+        # project manager and team member
+
+        pass
+
+    def tearDown(self):
+        # tear down up 4 users with specified roles
+        pass
+
+    def test_normal_user(self):
+        # read public projects
+
+        # read public groups
+
+        # read public tasks (still needed for tutorial)
+
+        # write to results based on user_id
+
+        pass
+
+    def test_team_member(self):
+        pass
+
+    def test_project_manager(self):
+        pass
+
+    def test_project_manager_and_team_member(self):
+        pass
