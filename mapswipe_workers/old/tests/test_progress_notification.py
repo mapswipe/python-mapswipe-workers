7b878234edf48bcedd74bbf08d0c2b80d7ca65fc
h9b <Hagellach37@users.noreply.github.com>
2020-08-04 11:41:48 +0200

    minor fixes #395

--- mapswipe_workers/old/tests/test_progress_notification.py
+++
@@ -1,5 +0,0 @@
-from mapswipe_workers.utils.slack_helper import send_progress_notification
-
-
-project_ids = ["build_area_default_with_bing"]
-[send_progress_notification(project_id) for project_id in project_ids]
--- mapswipe_workers/old/tests/test_tasking_manager_geometries.py
+++
@@ -1,5 +0,0 @@
-from mapswipe_workers.generate_stats import tasking_manager_geometries as t
-
-
-project_id = "-LqcAyzHkFF1_cu2tIpf"
-tasking_manager_geoms = t.generate_tasking_manager_geometries(project_id)
