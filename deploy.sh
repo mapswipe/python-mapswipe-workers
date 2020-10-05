6791644d233f7bfc8d2c85a35ddf6230e9199c9c
matthias_schaub <schaubm@posteo.de>
2020-10-05 09:47:18 +0200

    deploy.sh now runs ansible cmd. Travis will only run deploy when on branch dev. Docker recreate commands are now in recreate_container.sh.

--- deploy.sh
+++ deploy.sh
@@ -1,21 +1,4 @@
 #!/bin/bash
-# This script builds and starts all Docker container for running the Mapswipe ecosystem.
-# It is run either manually or by an Ansible Playbook after a successful Travis build.
-
-# docker-compose build --no-cache postgres firebase_deploy mapswipe_workers manager_dashboard nginx api
-docker-compose build --no-cache postgres mapswipe_workers manager_dashboard nginx api
-if [[ $? = 0 ]]; then
-    echo "success"
-else
-    echo "failure: $?"
-    exit
-fi
-
-# docker-compose up -d --force-recreate postgres firebase_deploy mapswipe_workers manager_dashboard nginx api
-docker-compose up -d --force-recreate postgres mapswipe_workers manager_dashboard nginx api
-if [[ $? = 0 ]]; then
-    echo "success"
-else
-    echo "failure: $?"
-    exit
-fi
+# Deploy to dev server.
+# This script is called by Travis.
+ansible-playbook -i ansible/inventory.yml ansible/playbook.yml
