a061f94b798470f0c8b6ad2dcc8d4eac11c79837
matthias_schaub <schaubm@posteo.de>
2020-11-01 13:08:30 +0100

    Substitude travis wait with custom funtion in deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 # Deploy to dev server.
 # This script is called by Travis.
+
+function bell() {
+  while true; do
+    echo -e "\a"
+    sleep 60
+  done
+}
+bell &
+
 ansible-playbook -i ansible/inventory.yml ansible/playbook.yml
+
+exit $?
