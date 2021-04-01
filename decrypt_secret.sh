96312762d9f474a69880371207d0358c862ccfc1
Hagellach37 <Hagellach37@users.noreply.github.com>
2021-04-01 11:59:41 +0200

    change variable name

--- decrypt_secret.sh
+++ decrypt_secret.sh
@@ -3,5 +3,5 @@
 # Decrypt the file
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="$PASSPHRASE" \
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_PASSPHRASE" \
 --output ./mapswipe_workers/serviceAccountKey.json ci-mapswipe-63a528ed5bb0.json.gpg
