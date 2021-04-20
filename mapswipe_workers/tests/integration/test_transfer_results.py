9c4fa89f6c03a8afdf643208d063b1fb029ba072
Hagellach37 <Hagellach37@users.noreply.github.com>
2021-04-20 22:20:39 +0200

    better test for results that have been submitted when tranfer results is running already

--- mapswipe_workers/tests/integration/test_transfer_results.py
+++ mapswipe_workers/tests/integration/test_transfer_results.py
@@ -86,7 +86,7 @@ class TestTranserResultsProject(unittest.TestCase):
         result = pg_db.retr_query(sql_query)
         self.assertIsNotNone(result)
 
-    def test_additional_results_are_not_deleted(self):
+    def test_additional_results_for_different_group_are_not_deleted(self):
         """Test if new results are not deleted when workflow has already started.
 
         Here we check what happens to results that are submitted,
@@ -100,10 +100,13 @@ class TestTranserResultsProject(unittest.TestCase):
         results_ref = fb_db.reference(f"v2/results/{self.project_id}")
         results = results_ref.get()
 
-        # add additional results in Firebase for group with key 'g120'
-        # These results should not get deleted by the function below
+        # Add additional results in Firebase for group with key 'g115'.
+        # This is the same group for which results already exist.
+        # So here we simulate another user mapping a group
+        # that has been mapped before already.
+        # These results should not get deleted by the function below.
         test_dir = os.path.dirname(__file__)
-        fixture_name = "build_area_additional_results.json"
+        fixture_name = "build_area_additional_results_different_group.json"
         file_path = os.path.join(
             test_dir, "fixtures", "tile_map_service_grid", "results", fixture_name
         )
@@ -129,8 +132,59 @@ class TestTranserResultsProject(unittest.TestCase):
         self.assertIsNotNone(result)
 
         # check if new results for the group 'g120' are still in Firebase
-        ref = fb_db.reference("v2/results/{0}".format(self.project_id))
-        self.assertDictEqual(ref.get(shallow=True), {"g120": True})
+        ref = fb_db.reference(
+            f"v2/results/{self.project_id}/g120/test_build_area/results"
+        )
+        self.assertEqual(len(ref.get(shallow=True)), 252)
+
+    def test_additional_results_for_same_group_are_not_deleted(self):
+        """Test if new results are not deleted when workflow has already started.
+
+        Here we check what happens to results that are submitted,
+        when the transfer of results workflow is already running.
+        We expect that these new results should remain in Firebase
+        until the next run of transfer results.
+        """
+
+        fb_db = auth.firebaseDB()
+
+        results_ref = fb_db.reference(f"v2/results/{self.project_id}")
+        results = results_ref.get()
+
+        # Add additional results in Firebase for group with key 'g115'.
+        # This is the same group for which results already exist.
+        # So here we simulate another user mapping a group
+        # that has been mapped before already.
+        # These results should not get deleted by the function below.
+        test_dir = os.path.dirname(__file__)
+        fixture_name = "build_area_additional_results_same_group.json"
+        file_path = os.path.join(
+            test_dir, "fixtures", "tile_map_service_grid", "results", fixture_name
+        )
+
+        with open(file_path) as test_file:
+            new_results = json.load(test_file)
+
+        results_ref = fb_db.reference(f"v2/results/{self.project_id}/g115/new_user/")
+        results_ref.set(new_results)
+
+        # run transfer results function
+        transfer_results_for_project(self.project_id, results)
+
+        # Check if results have been transferred
+        pg_db = auth.postgresDB()
+        sql_query = (
+            f"SELECT * "
+            f"FROM results "
+            f"WHERE project_id = '{self.project_id}' "
+            f"AND user_id = '{self.project_id}'"
+        )
+        result = pg_db.retr_query(sql_query)
+        self.assertIsNotNone(result)
+
+        # check if new results for the group 'g115' are still in Firebase
+        ref = fb_db.reference(f"v2/results/{self.project_id}/g115/new_user/results")
+        self.assertEqual(len(ref.get(shallow=True)), 252)
 
 
 if __name__ == "__main__":
