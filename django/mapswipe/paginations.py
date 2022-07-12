0c1bfc89359f7db4af38df1e84a24c423651638a
thenav56 <ayernavin@gmail.com>
2022-07-12 15:17:38 +0545

    Fix Pagination Query N+1 issue.

--- django/mapswipe/paginations.py
+++ django/mapswipe/paginations.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import copy
 from typing import Any, Generic, List, TypeVar
 
 import strawberry
+from asgiref.sync import sync_to_async
 from django.db.models import QuerySet
 from strawberry.arguments import UNSET
 from strawberry_django import utils
@@ -16,7 +18,13 @@ from strawberry_django.pagination import apply as apply_pagination
 
 class CountBeforePaginationMonkeyPatch(StrawberryDjangoPagination):
     def get_queryset(self, queryset: QuerySet[Any], info, pagination=UNSET, **kwargs):
-        self.count = queryset.count()
+        _current_queryset = copy.copy(queryset)
+
+        @sync_to_async
+        def _get_count():
+            return _current_queryset.count()
+
+        self.count_callback = _get_count
         queryset = apply_pagination(pagination, queryset)
         return super(StrawberryDjangoPagination, self).get_queryset(
             queryset, info, **kwargs
@@ -26,13 +34,30 @@ class CountBeforePaginationMonkeyPatch(StrawberryDjangoPagination):
 StrawberryDjangoPagination.get_queryset = CountBeforePaginationMonkeyPatch.get_queryset
 OffsetPaginationInput.limit = 1  # TODO: This is not working
 
-E = TypeVar("E")
+DjangoModelTypeVar = TypeVar("DjangoModelTypeVar")
 
 
 @strawberry.type
-class CountList(Generic[E]):
-    count: int
-    items: List[E]
+class CountList(Generic[DjangoModelTypeVar]):
+    @staticmethod
+    def resolve_count(root):
+        return root.node["count_callback"]()
+
+    @staticmethod
+    async def resolve_items(root):
+        @sync_to_async
+        def _get_list():
+            return list(root.node["queryset"])
+
+        items = await _get_list()
+        return items
+
+    count: int = strawberry.field(resolver=resolve_count)
+    items: List[DjangoModelTypeVar] = strawberry.field(resolver=resolve_items)
+
+    def __init__(self, *args, **kwargs):
+        self.node = kwargs.pop("node", {})
+        super().__init__(*args, **kwargs)
 
 
 class StrawberryDjangoCountList(StrawberryDjangoField):
@@ -56,8 +81,10 @@ class StrawberryDjangoCountList(StrawberryDjangoField):
     def resolver(self, info, source, **kwargs):
         qs = super().resolver(info, source, **kwargs)
         return CountList[self._base_type](
-            count=self.count,
-            items=list(qs),  # FIXME:
+            node=dict(
+                count_callback=self.count_callback,
+                queryset=qs,
+            )
         )
 
     def get_queryset(self, queryset, info, order=UNSET, **kwargs):
