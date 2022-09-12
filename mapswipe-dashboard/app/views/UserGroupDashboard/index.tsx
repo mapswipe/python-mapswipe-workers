6abb359f0ef3e486c569b458695710986bb97208
Sameer Rai <sameerai2736@gmail.com>
2022-09-12 17:24:18 +0545

    Show type and archive status in user/user group select

--- mapswipe-dashboard/app/views/UserGroupDashboard/index.tsx
+++ mapswipe-dashboard/app/views/UserGroupDashboard/index.tsx
@@ -85,21 +85,6 @@ interface Props {
     className?: string;
 }
 
-function downloadFile(data: BlobPart, fileName: string, fileType: string) {
-    const blob = new Blob([data], { type: fileType });
-
-    const a = document.createElement('a');
-    a.download = fileName;
-    a.href = window.URL.createObjectURL(blob);
-    const clickEvt = new MouseEvent('click', {
-        view: window,
-        bubbles: true,
-        cancelable: true,
-    });
-    a.dispatchEvent(clickEvt);
-    a.remove();
-}
-
 function UserGroupDashboard(props: Props) {
     const { className } = props;
 
