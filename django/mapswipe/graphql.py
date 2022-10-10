43f821b91d7e4ff43c9650812439bb03c41f8b1a
thenav56 <ayernavin@gmail.com>
2022-10-10 17:15:53 +0545

    Integrate sentry to mapswipe Django instance.

--- django/mapswipe/graphql.py
+++ django/mapswipe/graphql.py
@@ -1,11 +1,12 @@
-from typing import Any, Optional, Union
+from typing import Any, Union
 
+import sentry_sdk
 import strawberry
 from apps.existing_database.query import Query as ExistingDatabaseQuery
 from starlette.requests import Request
-from starlette.responses import Response
 from starlette.websockets import WebSocket
 from strawberry.django.views import AsyncGraphQLView
+from strawberry.types import ExecutionResult
 
 from .dataloaders import GobalDataLoader
 
@@ -16,7 +17,7 @@ class CustomAsyncGraphQLView(AsyncGraphQLView):
     async def get_context(
         self,
         request: Union[Request, WebSocket],
-        response: Optional[Response],
+        **_,
     ) -> Any:
         return {
             "request": request,
@@ -31,7 +32,22 @@ class Query(
     pass
 
 
-schema = strawberry.Schema(
+class Schema(strawberry.Schema):
+    def _scope_with_sentry(self, execute_func, *args, **kwargs) -> ExecutionResult:
+        operation_name = kwargs.get("operation_name")
+        with sentry_sdk.configure_scope() as scope:
+            scope.set_tag("kind", operation_name)
+            scope.transaction.name = operation_name
+            return execute_func(*args, **kwargs)
+
+    def execute_sync(self, *args, **kwargs) -> ExecutionResult:
+        return self._scope_with_sentry(super().execute_sync, *args, **kwargs)
+
+    def execute(self, *args, **kwargs) -> ExecutionResult:
+        return self._scope_with_sentry(super().execute, *args, **kwargs)
+
+
+schema = Schema(
     query=Query,
     mutation=None,
     extensions=[
--- django/mapswipe/utils.py
+++ django/mapswipe/utils.py
@@ -1,1 +1,58 @@
-pass
+import os
+
+
+class InvalidGitRepository(Exception):
+    pass
+
+
+def fetch_git_sha(path, head=None):
+    """
+    Source:
+    https://github.com/getsentry/raven-python/blob/03559bb05fd963e2be96372ae89fb0bce751d26d/raven/versioning.py
+    >>> fetch_git_sha(os.path.dirname(__file__))
+    """
+    if not head:
+        head_path = os.path.join(path, ".git", "HEAD")
+        if not os.path.exists(head_path):
+            raise InvalidGitRepository(
+                "Cannot identify HEAD for git repository at %s" % (path,)
+            )
+
+        with open(head_path, "r") as fp:
+            head = str(fp.read()).strip()
+
+        if head.startswith("ref: "):
+            head = head[5:]
+            revision_file = os.path.join(path, ".git", *head.split("/"))
+        else:
+            return head
+    else:
+        revision_file = os.path.join(path, ".git", "refs", "heads", head)
+
+    if not os.path.exists(revision_file):
+        if not os.path.exists(os.path.join(path, ".git")):
+            raise InvalidGitRepository(
+                "%s does not seem to be the root of a git repository" % (path,)
+            )
+
+        # Check for our .git/packed-refs' file since a `git gc` may have run
+        # https://git-scm.com/book/en/v2/Git-Internals-Maintenance-and-Data-Recovery
+        packed_file = os.path.join(path, ".git", "packed-refs")
+        if os.path.exists(packed_file):
+            with open(packed_file) as fh:
+                for line in fh:
+                    line = line.rstrip()
+                    if line and line[:1] not in ("#", "^"):
+                        try:
+                            revision, ref = line.split(" ", 1)
+                        except ValueError:
+                            continue
+                        if ref == head:
+                            return str(revision)
+
+        raise InvalidGitRepository(
+            'Unable to find ref to head "%s" in repository' % (head,)
+        )
+
+    with open(revision_file) as fh:
+        return str(fh.read()).strip()
