efd83532dfc0c699a7ccc9620efec88162082f7e
Hagellach37 <Hagellach37@users.noreply.github.com>
2022-10-10 21:07:00 +0200

    drop indexes and add new table for mapping_sessions_results

--- postgres/scripts/v2_to_v3/01_create_test_replica.sql
+++ postgres/scripts/v2_to_v3/01_create_test_replica.sql
@@ -4,36 +4,39 @@
  * to a new schema called 'replica_mini'.
  */
 
+drop schema "replica_mini" cascade;
 create schema "replica_mini";
+
 drop table if exists "replica_mini".projects cascade;
 drop table if exists "replica_mini".results cascade;
 drop table if exists "replica_mini".groups cascade;
 drop table if exists "replica_mini".tasks cascade;
 drop table if exists "replica_mini".users cascade;
 
+
 ------------------------------------------
 -- create tables for mini replica
 -------------------------------------------
 
 CREATE TABLE IF NOT EXISTS "replica_mini".projects (
-   created timestamp,
-   created_by varchar,
-   geom geometry,
-   image varchar,
-   is_featured boolean,
-   look_for varchar,
-   name varchar,
-   progress int,
-   project_details varchar,
-   project_id varchar,
-   project_type int,
-   required_results int,
-   result_count int,
-   status varchar,
-   verification_number int,
-   project_type_specifics json,
-   PRIMARY KEY (project_id)
-    );
+    created timestamp,
+    created_by varchar,
+    geom geometry,
+    image varchar,
+    is_featured boolean,
+    look_for varchar,
+    name varchar,
+    progress int,
+    project_details varchar,
+    project_id varchar,
+    project_type int,
+    required_results int,
+    result_count int,
+    status varchar,
+    verification_number int,
+    project_type_specifics json,
+    PRIMARY KEY (project_id)
+);
 
 CREATE TABLE IF NOT EXISTS "replica_mini".groups (
     project_id varchar,
@@ -45,7 +48,7 @@ CREATE TABLE IF NOT EXISTS "replica_mini".groups (
     project_type_specifics json,
     PRIMARY KEY (project_id, group_id),
     FOREIGN KEY (project_id) REFERENCES "replica_mini".projects (project_id)
-    );
+);
 
 CREATE INDEX IF NOT EXISTS groups_projectid ON "replica_mini".groups
     USING btree (group_id);
@@ -62,7 +65,7 @@ CREATE TABLE IF NOT EXISTS "replica_mini".tasks (
     PRIMARY KEY (project_id, group_id, task_id),
     FOREIGN KEY (project_id) REFERENCES "replica_mini".projects (project_id),
     FOREIGN KEY (project_id, group_id) REFERENCES "replica_mini".groups (project_id, group_id)
-    );
+);
 
 CREATE INDEX IF NOT EXISTS tasks_task_id ON "replica_mini".tasks
     USING btree (task_id);
@@ -78,12 +81,12 @@ CREATE TABLE IF NOT EXISTS "replica_mini".users (
     username varchar,
     created timestamp,
     PRIMARY KEY (user_id)
-    );
+);
 
 CREATE INDEX IF NOT EXISTS users_userid ON "replica_mini".users
     USING btree (user_id);
 
--- Do we need this table?!
+   -- Do we need this table?!
 CREATE TABLE IF NOT EXISTS "replica_mini".users_temp (
     user_id varchar,
     username varchar,
@@ -104,7 +107,7 @@ CREATE TABLE IF NOT EXISTS "replica_mini".results (
     FOREIGN KEY (project_id, group_id) REFERENCES "replica_mini".groups (project_id, group_id),
     FOREIGN KEY (project_id, group_id, task_id) REFERENCES "replica_mini".tasks (project_id, group_id, task_id),
     FOREIGN KEY (user_id) REFERENCES "replica_mini".users (user_id)
-    );
+);
 
 CREATE INDEX IF NOT EXISTS results_projectid ON "replica_mini".results
     USING btree (project_id);
@@ -139,58 +142,58 @@ CREATE TABLE IF NOT EXISTS "replica_mini".results_temp (
 -------------------------------------------
 
 insert into "replica_mini".projects (
-    select
-        *
-    from projects p
-    where created > '2022-05-01'
-        limit 10
+	select
+		*
+	from projects p 
+	where created > '2022-05-01'
+	limit 10
 );
 
 insert into "replica_mini".groups
-    (
-        select g.*
-        from groups g, "replica_mini".projects p
-        where g.project_id = p.project_id
-    );
+(
+select g.*
+from groups g, "replica_mini".projects p
+where g.project_id = p.project_id
+);
 
 
 insert into "replica_mini".tasks
-    (
-        select t.*
-        from tasks t, "replica_mini".projects p
-        where t.project_id = p.project_id
-    );
+(
+select t.*
+from tasks t, "replica_mini".projects p
+where t.project_id = p.project_id
+);
 
 insert into "replica_mini".users
-    (
-        select *
-        from users u
-    );
+(
+select *
+from users u 
+);
 
 insert into "replica_mini".results
-    (
-        select r.*
-        from results r, "replica_mini".projects p
-        where r.project_id = p.project_id
-    );
+(
+select r.*
+from results r, "replica_mini".projects p
+where r.project_id = p.project_id
+);
 
 ------------------------------------------
--- check table sizes
+-- check tables
 -------------------------------------------
 
 SELECT
-    schema_name,
-    relname,
-    pg_size_pretty(table_size) AS size,
+  schema_name,
+  relname,
+  pg_size_pretty(table_size) AS size,
   table_size
 FROM (
-    SELECT
-    pg_catalog.pg_namespace.nspname           AS schema_name,
-    relname,
-    pg_relation_size(pg_catalog.pg_class.oid) AS table_size
-    FROM pg_catalog.pg_class
-    JOIN pg_catalog.pg_namespace ON relnamespace = pg_catalog.pg_namespace.oid
-    ) t
+       SELECT
+         pg_catalog.pg_namespace.nspname           AS schema_name,
+         relname,
+         pg_relation_size(pg_catalog.pg_class.oid) AS table_size
+       FROM pg_catalog.pg_class
+         JOIN pg_catalog.pg_namespace ON relnamespace = pg_catalog.pg_namespace.oid
+     ) t
 WHERE schema_name = 'replica_mini' AND table_size > 0 ORDER BY table_size DESC;
 
 
