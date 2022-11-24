c73c921e752e4b2b3abb43e197b6702530d278f3
Laurent Savaete <laurent@where.tf>
2022-11-24 18:38:40 +0100

    Cleanup data migration file

--- postgres/scripts/v2_to_v3/04_transfer_results_into_new_tables.sql
+++ postgres/scripts/v2_to_v3/04_transfer_results_into_new_tables.sql
@@ -1,12 +1,12 @@
 /*
  * This script takes the data from the 'results' table
- * and transfers them to the new tables 
+ * and transfers them to the new tables
  * - 'mapping_sessions' and
  * - 'mapping_sessions_results'
  */
 set search_path = 'public';
 
-insert into mapping_sessions 
+insert into mapping_sessions
 (
 select
   project_id
@@ -16,9 +16,7 @@ select
   ,Min(start_time) as start_time
   ,Max(end_time) as end_time
   ,count(*) as items_count
-from (
-	select * from results -- limit 100000
-) as foo
+from results
 group by project_id, group_id, user_id
 )
 on conflict do nothing;
@@ -27,19 +25,17 @@ on conflict do nothing;
 insert into mapping_sessions_results
 (
 select
-	m.mapping_session_id
-	,r.task_id 
-	,r."result" 
+    m.mapping_session_id
+    ,r.task_id
+    ,r."result"
 from results r, mapping_sessions m
-where 
-	r.project_id = m.project_id and
-	r.group_id = m.group_id and
-	r.user_id = m.user_id 
+where
+    r.project_id = m.project_id and
+    r.group_id = m.group_id and
+    r.user_id = m.user_id
 )
 on conflict do nothing;
 
-select * from mapping_sessions;
-select * from mapping_sessions_results;
-
-
-
+select count(*) from results;
+select sum(items_count) from mapping_sessions;
+select count(*) from mapping_sessions_results;
