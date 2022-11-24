df88908719104dfc145390c4056a855b582a7fab
Hagellach37 <Hagellach37@users.noreply.github.com>
2022-11-24 15:11:31 +0100

    adjust test for invalid results

--- mapswipe_workers/tests/integration/test_transfer_results_invalid_results.py
+++ mapswipe_workers/tests/integration/test_transfer_results_invalid_results.py
@@ -4,19 +4,51 @@ import unittest
 
 import set_up
 import tear_down
+from base import BaseTestCase
 
 from mapswipe_workers import auth
 from mapswipe_workers.firebase_to_postgres import transfer_results
 
 
-class TestTranserResultsProject(unittest.TestCase):
+class TestTranserResultsProject(BaseTestCase):
     def setUp(self):
+        super().setUp()
+        project_type = "tile_map_service_grid"
+        fixture_name = "build_area"
         self.project_id = set_up.create_test_project(
-            "tile_map_service_grid", "build_area", results=False
+            project_type, fixture_name, results=False
         )
 
     def tearDown(self):
         tear_down.delete_test_data(self.project_id)
+
+    def verify_mapping_results_in_postgres(self):
+        """Check that mapping_sessions and mapping_sessions_results
+        contain the expected data.
+        """
+        pg_db = auth.postgresDB()
+        expected_items_count = 252
+
+        sql_query = (
+            f"SELECT * "
+            f"FROM mapping_sessions "
+            f"WHERE project_id = '{self.project_id}' "
+            f"AND user_id = '{self.project_id}'"
+        )
+        result = pg_db.retr_query(sql_query)
+        self.assertEqual(len(result), 1)
+        self.assertEqual(result[0][6], expected_items_count)
+
+        q2 = (
+            "SELECT msr.* "
+            "FROM mapping_sessions_results msr "
+            "JOIN mapping_sessions ms ON "
+            "ms.mapping_session_id = msr.mapping_session_id "
+            f"WHERE ms.project_id = '{self.project_id}' "
+            f"AND ms.user_id = '{self.project_id}'"
+        )
+        result2 = pg_db.retr_query(q2)
+        self.assertEqual(len(result2), expected_items_count)
 
     def test_invalid_task_in_result(self):
         """Test for results that are not valid.
@@ -41,6 +73,8 @@ class TestTranserResultsProject(unittest.TestCase):
         ref.set(test_data)
 
         transfer_results.transfer_results(project_id_list=[self.project_id])
+
+        self.verify_mapping_results_in_postgres()
 
         fb_db = auth.firebaseDB()
         ref = fb_db.reference(
