2f28c360450dd56a7f9ebd90d0a832d22c4ca497
Oliver Fritz <oliver.fritz@heigit.org>
2022-12-07 16:39:13 +0100

    projectTypeOptions config for crowdmap webapp

--- manager-dashboard/app/Base/configs/projectTypes.ts
+++ manager-dashboard/app/Base/configs/projectTypes.ts
@@ -8,7 +8,7 @@ import {
 
 const PROJECT_CONFIG_NAME = process.env.REACT_APP_PROJECT_CONFIG_NAME as string;
 
-const defaultProjectTypeOptions: {
+const mapswipeProjectTypeOptions: {
     value: ProjectType;
     label: string;
 }[] = [
@@ -18,16 +18,14 @@ const defaultProjectTypeOptions: {
     { value: PROJECT_TYPE_COMPLETENESS, label: 'Completeness' },
 ];
 
-const developmentProjectTypeOptions: {
+const crowdmapProjectTypeOptions: {
     value: ProjectType;
     label: string;
 }[] = [
     { value: PROJECT_TYPE_BUILD_AREA, label: 'Build Area' },
-    { value: PROJECT_TYPE_FOOTPRINT, label: 'Footprint' },
-    { value: PROJECT_TYPE_CHANGE_DETECTION, label: 'Change Detection' },
     { value: PROJECT_TYPE_COMPLETENESS, label: 'Completeness' },
 ];
 
-const projectTypeOptions = PROJECT_CONFIG_NAME === 'development' ? developmentProjectTypeOptions : defaultProjectTypeOptions;
+const projectTypeOptions = PROJECT_CONFIG_NAME === 'crowdmap' ? crowdmapProjectTypeOptions : mapswipeProjectTypeOptions;
 
 export default projectTypeOptions;
