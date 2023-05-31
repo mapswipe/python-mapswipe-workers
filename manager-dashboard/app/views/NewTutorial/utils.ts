01e4bf526cb3047e20aaa5d9f4c665e5d21d5039
shreeyash07 <shresthashree0807@gmail.com>
2023-05-31 17:25:03 +0545

    Add schema for define options form

--- manager-dashboard/app/views/NewTutorial/utils.ts
+++ manager-dashboard/app/views/NewTutorial/utils.ts
@@ -20,6 +20,7 @@ import {
     PROJECT_TYPE_BUILD_AREA,
     PROJECT_TYPE_CHANGE_DETECTION,
     PROJECT_TYPE_COMPLETENESS,
+    PROJECT_TYPE_FOOTPRINT,
 } from '#utils/common';
 
 // FIXME: include here
@@ -36,6 +37,19 @@ export type TutorialTasks = GeoJSON.FeatureCollection<GeoJSON.Geometry, {
     // eslint-disable-next-line
     tile_z: number;
 }>;
+
+export type DefineOptions = {
+    option: number;
+    title: string;
+    description: string;
+    icon: string;
+    iconColor: string;
+    reasons: {
+        reason: number;
+        description: string;
+        icon: string;
+    }[];
+}[];
 
 export interface TutorialFormType {
     lookFor: string;
@@ -65,6 +79,7 @@ export interface TutorialFormType {
     projectType: ProjectType;
     tileServerB?: TileServer,
     zoomLevel?: number;
+    defineOptions?: DefineOptions;
 }
 
 export type PartialTutorialFormType = PartialForm<
@@ -73,7 +88,7 @@ export type PartialTutorialFormType = PartialForm<
         exampleImage2?: File;
     },
     // NOTE: we do not want to change File and FeatureCollection to partials
-    'scenario' | 'tutorialTasks' | 'exampleImage1' | 'exampleImage2'
+    'scenario' | 'tutorialTasks' | 'exampleImage1' | 'exampleImage2' | 'option' | 'reason'
 >;
 
 type TutorialFormSchema = ObjectSchema<PartialTutorialFormType>;
@@ -85,6 +100,13 @@ type ScreenFormSchemaFields = ReturnType<ScreenSchema['fields']>;
 
 type ScreenFormSchema = ArraySchema<ScreenType, PartialTutorialFormType>;
 type ScreenFormSchemaMember = ReturnType<ScreenFormSchema['member']>;
+
+type DefineOptionsType = NonNullable<PartialTutorialFormType['defineOptions']>[number];
+type DefineOptionsSchema = ObjectSchema<DefineOptionsType, PartialTutorialFormType>;
+type DefineOptionsSchemaFields = ReturnType<DefineOptionsSchema['fields']>
+
+type DefineOptionsFormSchema = ArraySchema<DefineOptionsType, PartialTutorialFormType>;
+type DefineOptionsFormSchemaMember = ReturnType<DefineOptionsFormSchema['member']>;
 
 export const tutorialFormSchema: TutorialFormSchema = {
     fields: (value): TutorialFormSchemaFields => {
@@ -166,6 +188,67 @@ export const tutorialFormSchema: TutorialFormSchema = {
             exampleImage1: { required: true },
             exampleImage2: { required: true },
         };
+        baseSchema = addCondition(
+            baseSchema,
+            value,
+            ['projectType'],
+            ['defineOptions'],
+            (v) => {
+                const defineOptionsField: DefineOptionsFormSchema = {
+                    keySelector: (key) => key.option,
+                    member: (): DefineOptionsFormSchemaMember => ({
+                        fields: (): DefineOptionsSchemaFields => ({
+                            option: {
+                                required: true,
+                            },
+                            title: {
+                                required: true,
+                                requiredValidation: requiredStringCondition,
+                                validations: [getNoMoreThanNCharacterCondition(500)],
+                            },
+                            description: {
+                                required: true,
+                                requiredValidation: requiredStringCondition,
+                                validations: [getNoMoreThanNCharacterCondition(500)],
+                            },
+                            icon: {
+                                required: true,
+                            },
+                            iconColor: {
+                                required: true,
+                            },
+                            reasons: {
+                                keySelector: (key) => key.reason,
+                                member: () => ({
+                                    fields: () => ({
+                                        reason: {
+                                            required: true,
+                                        },
+                                        description: {
+                                            required: true,
+                                            requiredValidation: requiredStringCondition,
+                                            validations: [getNoMoreThanNCharacterCondition(500)],
+                                        },
+                                        icon: {
+                                            required: true,
+                                        },
+                                    }),
+                                }),
+                            },
+                        }),
+                    }),
+                };
+
+                if (v?.projectType === PROJECT_TYPE_FOOTPRINT) {
+                    return {
+                        defineOptions: defineOptionsField,
+                    };
+                }
+                return {
+                    defineOptions: { forceValue: nullValue },
+                };
+            },
+        );
         baseSchema = addCondition(
             baseSchema,
             value,
