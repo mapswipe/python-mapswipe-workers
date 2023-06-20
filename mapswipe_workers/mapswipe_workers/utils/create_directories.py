a9abbbba11293dc320524bbfb4c2edb89eac2b0c
jstier <stier.jochen@web.de>
2023-06-20 10:05:46 +0200

    move dirs back into function

--- mapswipe_workers/mapswipe_workers/utils/create_directories.py
+++ mapswipe_workers/mapswipe_workers/utils/create_directories.py
@@ -3,23 +3,22 @@ import pathlib
 
 from mapswipe_workers.definitions import DATA_PATH
 
-dirs = (
-    "/api",
-    "/api/agg_results",
-    "/api/groups",
-    "/api/history",
-    "/api/hot_tm",
-    "/api/project_geometries",
-    "/api/projects",
-    "/api/results",
-    "/api/tasks",
-    "/api/users",
-    "/api/yes_maybe",
-)
-
 
 def create_directories() -> None:
     """Create directories"""
+    dirs = (
+        "/api",
+        "/api/agg_results",
+        "/api/groups",
+        "/api/history",
+        "/api/hot_tm",
+        "/api/project_geometries",
+        "/api/projects",
+        "/api/results",
+        "/api/tasks",
+        "/api/users",
+        "/api/yes_maybe",
+    )
 
     for dir_name in dirs:
         path = pathlib.Path(DATA_PATH + dir_name)
