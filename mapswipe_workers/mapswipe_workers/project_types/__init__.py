2f3d523a03abf6b0e247e02cfb880200c56e89a1
Oliver Fritz <oliver.fritz@heigit.org>
2024-11-28 16:47:12 +0100

    style: isort

--- mapswipe_workers/mapswipe_workers/project_types/__init__.py
+++ mapswipe_workers/mapswipe_workers/project_types/__init__.py
@@ -2,13 +2,13 @@ from .arbitrary_geometry.digitization.project import DigitizationProject
 from .arbitrary_geometry.footprint.project import FootprintProject
 from .arbitrary_geometry.footprint.tutorial import FootprintTutorial
 from .media_classification.project import MediaClassificationProject
+from .street.project import StreetProject
 from .tile_map_service.change_detection.project import ChangeDetectionProject
 from .tile_map_service.change_detection.tutorial import ChangeDetectionTutorial
 from .tile_map_service.classification.project import ClassificationProject
 from .tile_map_service.classification.tutorial import ClassificationTutorial
 from .tile_map_service.completeness.project import CompletenessProject
 from .tile_map_service.completeness.tutorial import CompletenessTutorial
-from .street.project import StreetProject
 
 __all__ = [
     "ClassificationProject",
