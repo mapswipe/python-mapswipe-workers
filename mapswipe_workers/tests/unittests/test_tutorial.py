550df7a237036e9217e58d745f19293375da6453
Levi Szamek <levi@szamek.de>
2025-02-20 12:36:50 +0100

    fix: working integration test for tutorial creation

--- mapswipe_workers/tests/unittests/test_tutorial.py
+++ mapswipe_workers/tests/unittests/test_tutorial.py
@@ -1,22 +1,22 @@
 import os
 import unittest
 
-from mapswipe_workers.project_types import ClassificationTutorial
+from mapswipe_workers.project_types import StreetTutorial
 from tests.fixtures import FIXTURE_DIR, get_fixture
 
 
 class TestTutorial(unittest.TestCase):
     def test_init_tile_classification_project(self):
         tutorial_draft = get_fixture(
-            os.path.join(FIXTURE_DIR, "tutorialDrafts", "tile_classification.json")
+            os.path.join(FIXTURE_DIR, "tutorialDrafts", "street.json")
         )
-        self.assertIsNotNone(ClassificationTutorial(tutorial_draft=tutorial_draft))
+        self.assertIsNotNone(StreetTutorial(tutorial_draft=tutorial_draft))
 
     def test_create_tile_classification_tasks(self):
         tutorial_draft = get_fixture(
-            os.path.join(FIXTURE_DIR, "tutorialDrafts", "tile_classification.json")
+            os.path.join(FIXTURE_DIR, "tutorialDrafts", "street.json")
         )
-        tutorial = ClassificationTutorial(tutorial_draft=tutorial_draft)
+        tutorial = StreetTutorial(tutorial_draft=tutorial_draft)
         tutorial.create_tutorial_groups()
         tutorial.create_tutorial_tasks()
         self.assertTrue(tutorial.groups)
